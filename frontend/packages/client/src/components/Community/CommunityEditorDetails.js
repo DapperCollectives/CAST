729de9d0eb1197eb6cc79a2f809b5c0689b9c352
German Urrustarazu <gerurr-a296@4sided.com>
2022-05-26 16:50:39 -0300

    remove commented code

--- frontend/packages/client/src/components/Community/CommunityEditorDetails.js
+++ frontend/packages/client/src/components/Community/CommunityEditorDetails.js
@@ -148,30 +148,6 @@ export const CommunityUsersForm = ({
   );
 };
 
-// const uniqueElements = (adminList) => {
-//   const setList = new Set([
-//     ...adminList.map((e) => e.addr.toLocaleLowerCase()),
-//   ]);
-//   return adminList.length === setList.size;
-// };
-
-// const hasValidAddresses = (list) => {
-//   return list.every((el) => /0[x,X][a-zA-Z0-9]{16}$/gim.test(el.addr));
-// };
-// const listHasChanged = (adminList, admins) => {
-//   const setList = new Set([...adminList.map((e) => e.addr)]);
-
-//   if (admins.length !== setList.size) return true;
-
-//   for (let admin of admins) if (!setList.has(admin)) return true;
-
-//   return false;
-// };
-
-// const notEmptyAddr = (list) => {
-//   return list.every((e) => e.addr.trim().length > 0);
-// };
-
 const CommunityMembersEditor = ({
   title = "Admins",
   description = "",
