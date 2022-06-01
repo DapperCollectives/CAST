b701b6bc6434d825d67feb88e65bce9ba5e0ee86
Richard Melkonian <rvm@Richards-MacBook-Pro.local>
2022-06-01 20:41:46 +0100

    ammended migration

--- backend/migrations/000013_add_community_categories_and_toc.down.sql
+++ backend/migrations/000013_add_community_categories_and_toc.down.sql
@@ -1,5 +1,5 @@
-ALTER TABLE communities DROP COLUMN category;
-ALTER TABLE communities DROP COLUMN terms_and_conditions_url;
-ALTER TABLE communities DROP COLUMN only_authors_to_submit;
+ALTER TABLE communities DROP COLUMN IF EXISTS category;
+ALTER TABLE communities DROP COLUMN IF EXISTS terms_and_conditions_url;
+ALTER TABLE communities DROP COLUMN IF EXISTS only_authors_to_submit;
 DROP TABLE IF EXISTS community_types;
 DROP TYPE IF EXISTS community_type;
--- backend/migrations/000015_alter_communities.down.sql
+++ backend/migrations/000015_alter_communities.down.sql
@@ -1,4 +1,4 @@
 ALTER TABLE communities DROP COLUMN IF EXISTS contract_name;
 ALTER TABLE communities DROP COLUMN IF EXISTS contract_addr;
-ALTER TABLE communities DROP COLUMN IF EXISTS Spublic_path;
+ALTER TABLE communities DROP COLUMN IF EXISTS public_path;
 ALTER TABLE communities DROP COLUMN IF EXISTS threshold;
