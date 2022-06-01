b4abf3c19ac8feff7ecce88a00655780a10f7685
jacksonfoley <jackson.foley@dapperlabs.com>
2022-06-01 16:17:45 -0600

    commit alter type tx before using enum type. remove token weighted capped from voting_strategies

--- backend/migrations/000014_add_one_address_strategy.up.sql
+++ backend/migrations/000014_add_one_address_strategy.up.sql
@@ -1,4 +1,8 @@
-ALTER TYPE strategies ADD VALUE 'one-address-one-vote';
+BEGIN;
+ALTER TYPE strategies ADD VALUE IF NOT EXISTS 'one-address-one-vote';
+END TRANSACTION;
+COMMIT;
+
 INSERT INTO voting_strategies (key, name, description)
 VALUES ('one-address-one-vote', 'One Address One Vote', 'one address is simply only allowed one vote, assets do not come into play.');
 
