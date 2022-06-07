4cd8409b25228b24546d797a20d35c186b0ecd11
German Urrustarazu <germanurrus@gmail.com>
2022-06-07 15:28:58 -0300

    temp changes

--- frontend/packages/client/src/components/CommunityCreate/StepTwo.js
+++ frontend/packages/client/src/components/CommunityCreate/StepTwo.js
@@ -1,9 +1,29 @@
 import React, { useEffect } from "react";
 import { CommunityUsersForm } from "../Community/CommunityEditorDetails";
 import useFlowAddrValidator from "../Community/hooks/useFlowAddrValidator";
+import { useModalContext } from "contexts/NotificationModal";
 
 const isInitialList = (listAddr) => {
   return listAddr?.length === 1 && listAddr[0].addr === "";
+};
+
+const HelpModal = () => {
+  return (
+    <div className="columns m-0 p-0 is-multiline is-mobile">
+      <div className="column is-full m-0 p-0 is-flex is-justify-content-center py-5">
+        <div
+          className="rounded-full is-size-2 has-text-white is-flex is-align-items-center is-justify-content-center"
+          style={{ height: 50, width: 50, background: "red" }}
+        >
+          X
+        </div>
+      </div>
+      <div className="column is-full p-0 m-0 divider pb-5 is-flex is-flex-direction-column is-align-items-center">
+        <p>{props.errorTitle}</p>
+        {props?.error && props.error}
+      </div>
+    </div>
+  );
 };
 export default function StepTwo({
   stepData,
@@ -12,6 +32,8 @@ export default function StepTwo({
   moveToNextStep,
   isStepValid,
 }) {
+  const { openModal, closeModal } = useModalContext();
+
   const { listAddrAdmins = [{ addr: "" }], listAddrAuthors = [{ addr: "" }] } =
     stepData || {};
 
@@ -76,11 +98,51 @@ export default function StepTwo({
     onDataChange({ listAddrAuthors: [...listAddrAuthors, { addr: "" }] });
   };
 
+  const openModalHelp = () => {
+    openModal(
+      React.createElement(Error, {
+        error: (
+          <p className="has-text-red">
+            <b>{response.error}</b>
+          </p>
+        ),
+        errorTitle: "Something went wrong with your vote.",
+      }),
+      {
+        classNameModalContent: "rounded-sm",
+      }
+    );
+  };
+
   return (
     <>
       <CommunityUsersForm
-        title="Admins"
-        description="The admins will be able to edit the space settings and moderate proposals. You must add one address per line."
+        title={
+          <>
+            Admins
+            <button
+              className="delete has-text-grey rounded-lg"
+              style={{ border: "none" }}
+            >
+              ?
+            </button>
+          </>
+        }
+        description={
+          <>
+            <p>
+              The admins will be able to edit the space settings and moderate
+              proposals. You must add one address per line. You must add one
+              address per line.
+            </p>
+            <p>
+              Admin addresses will be added automatically as authors and members
+              for the community. Community creator is set as default as admin
+              and member"
+            </p>
+            <p>Community creator is set as admin and member by default"</p>
+          </>
+        }
         addrList={listAddrAdmins}
         onAddressChange={onAdminAddressChange}
         onDeleteAddress={onAdminAddressDelete}
