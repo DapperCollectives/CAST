72d748a59f4c794f2c01b4c5d826e42c9f67b0eb
German Urrustarazu <germanurrus@gmail.com>
2022-06-07 16:40:58 -0300

    add modal

--- frontend/packages/client/src/components/CommunityCreate/StepTwo.js
+++ frontend/packages/client/src/components/CommunityCreate/StepTwo.js
@@ -9,22 +9,28 @@ const isInitialList = (listAddr) => {
 
 const HelpModal = () => {
   return (
-    <div className="columns m-0 p-0 is-multiline is-mobile">
-      <div className="column is-full m-0 p-0 is-flex is-justify-content-center py-5">
-        <div
-          className="rounded-full is-size-2 has-text-white is-flex is-align-items-center is-justify-content-center"
-          style={{ height: 50, width: 50, background: "red" }}
-        >
-          X
-        </div>
-      </div>
-      <div className="column is-full p-0 m-0 divider pb-5 is-flex is-flex-direction-column is-align-items-center">
-        <p>{props.errorTitle}</p>
-        {props?.error && props.error}
+    <div className="columns m-5 p-2 is-multiline is-mobile">
+      <div className="column is-full p-0 m-0 is-flex is-flex-direction-column is-align-items-flex-start is-justify-content-center">
+        <p className="has-text-grey small-text has-text-justified mb-2">
+          Admin addresses will be added automatically as authors and members for
+          the community.
+        </p>
+        <p className="has-text-grey small-text has-text-justified">
+          In addition, community creator address will be set as admin and member
+          by default.
+        </p>
       </div>
     </div>
   );
 };
+
+const buttonStyle = {
+  padding: "1px 4px",
+  border: "none",
+  fontSize: "12px",
+  marginLeft: "4px",
+};
+
 export default function StepTwo({
   stepData,
   setStepValid,
@@ -32,7 +38,7 @@ export default function StepTwo({
   moveToNextStep,
   isStepValid,
 }) {
-  const { openModal, closeModal } = useModalContext();
+  const { openModal } = useModalContext();
 
   const { listAddrAdmins = [{ addr: "" }], listAddrAuthors = [{ addr: "" }] } =
     stepData || {};
@@ -99,19 +105,9 @@ export default function StepTwo({
   };
 
   const openModalHelp = () => {
-    openModal(
-      React.createElement(Error, {
-        error: (
-          <p className="has-text-red">
-            <b>{response.error}</b>
-          </p>
-        ),
-        errorTitle: "Something went wrong with your vote.",
-      }),
-      {
-        classNameModalContent: "rounded-sm",
-      }
-    );
+    openModal(<HelpModal />, {
+      classNameModalContent: "rounded-sm",
+    });
   };
 
   return (
@@ -121,28 +117,15 @@ export default function StepTwo({
           <>
             Admins
             <button
-              className="delete has-text-grey rounded-lg"
-              style={{ border: "none" }}
+              className="delete has-text-grey rounded-lg cursor-pointer"
+              style={buttonStyle}
+              onClick={openModalHelp}
             >
               ?
             </button>
           </>
         }
-        description={
-          <>
-            <p>
-              The admins will be able to edit the space settings and moderate
-              proposals. You must add one address per line. You must add one
-              address per line.
-            </p>
-            <p>
-              Admin addresses will be added automatically as authors and members
-              for the community. Community creator is set as default as admin
-              and member"
-            </p>
-            <p>Community creator is set as admin and member by default"</p>
-          </>
-        }
+        description="The admins will be able to edit the space settings and moderate proposals. You must add one address per line. You must add one address per line."
         addrList={listAddrAdmins}
         onAddressChange={onAdminAddressChange}
         onDeleteAddress={onAdminAddressDelete}
