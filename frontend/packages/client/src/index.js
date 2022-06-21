ce31b6dfc03c4f778291ca6ba253988b2df665c5
German Urrustarazu <germanurrus@gmail.com>
2022-06-21 10:44:55 -0300

    update import

--- frontend/packages/client/src/index.js
+++ frontend/packages/client/src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// eslint-disable-next-line no-unused-vars
-import * as Buffer from "./polyfill";
+import "./polyfill";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
