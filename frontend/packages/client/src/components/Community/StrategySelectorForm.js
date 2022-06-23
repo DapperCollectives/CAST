67b90fe2db0892ae91fd3b92b2f7f961f0368c56
German Urrustarazu <germanurrus@gmail.com>
2022-06-23 13:49:28 -0300

    remove useEffect

--- frontend/packages/client/src/components/Community/StrategySelectorForm.js
+++ frontend/packages/client/src/components/Community/StrategySelectorForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useVotingStrategies } from 'hooks';
 import { AddButton } from 'components';
 import { Bin } from 'components/Svg';
