76bb0b9519569a5b146801cbd628afc2c876d2d2
Richard Melkonian <r.v.melkonian@gmail.com>
2022-06-28 19:50:53 +0100

    updated onlyAuthors check

--- backend/main/server/app.go
+++ backend/main/server/app.go
@@ -768,7 +768,7 @@ func (a *App) createProposal(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if *community.Only_authors_to_submit {
+	if *community.Only_authors_to_submit == true {
 		if err := models.EnsureRoleForCommunity(a.DB, p.Creator_addr, communityId, "author"); err != nil {
 			errMsg := fmt.Sprintf("account %s is not an author for community %d", p.Creator_addr, p.Community_id)
 			log.Error().Err(err).Msg(errMsg)
