8d1a3a30aaa0ba206f1ea45cd430e674e4c888a2
German Urrustarazu <germanurrus@gmail.com>
2022-06-29 18:33:55 -0300

    remove map function

--- frontend/packages/client/src/components/Community/CommunityPropsAndVoting.js
+++ frontend/packages/client/src/components/Community/CommunityPropsAndVoting.js
@@ -3,33 +3,6 @@ import ActionButton from 'components/ActionButton';
 import StrategySelectorForm from 'components/Community/StrategySelectorForm';
 import isEqual from 'lodash/isEqual';
 
-// this object is used to change
-// field names as are used
-// on the backend
-const fieldMapPayload = {
-  contractAddress: 'addr',
-  contractName: 'name',
-  maxWeight: 'maxWeight',
-  minimunBalance: 'threshold',
-  publicPath: 'publicPath',
-};
-
-// this function renames fields if necessary to prepare payload for backend
-const mapFieldsForBackend = (contract) => {
-  return Object.assign(
-    {},
-    ...Object.entries(contract).map(([key, value]) => ({
-      ...(fieldMapPayload[key] && value
-        ? {
-            [fieldMapPayload[key]]: value,
-          }
-        : value
-        ? { [key]: value }
-        : undefined),
-    }))
-  );
-};
-
 const hasListChanged = (newList, originalList) => {
   return !isEqual(newList, originalList);
 };
@@ -44,7 +17,7 @@ export default function CommunityProposalsAndVoting({
       .filter((st) => st?.toDelete !== true)
       .map((st) => ({
         name: st.name,
-        contract: mapFieldsForBackend(st.contract),
+        contract: st.contract,
       }));
     await updateCommunity({
       strategies: updatePayload,
