ea93097700425b38694b77d04ab914ae30ebb7b9
German Urrustarazu <germanurrus@gmail.com>
2022-07-06 12:11:28 -0300

    add react-query configuration

--- frontend/packages/client/src/App.js
+++ frontend/packages/client/src/App.js
@@ -9,6 +9,16 @@ import { ErrorHandler } from './components';
 import NotificationModalProvider from './contexts/NotificationModal';
 import { ErrorBoundary } from 'react-error-boundary';
 import AppPages from './pages';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+// create react-query client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
@@ -18,15 +28,17 @@ function App() {
       )}
       onReset={() => (window.location.href = '/')}
     >
-      <Web3Provider network={process.env.REACT_APP_FLOW_ENV}>
-        <NotificationModalProvider>
-          <Router>
-            <ErrorHandler>
-              <AppPages />
-            </ErrorHandler>
-          </Router>
-        </NotificationModalProvider>
-      </Web3Provider>
+      <QueryClientProvider client={queryClient}>
+        <Web3Provider network={process.env.REACT_APP_FLOW_ENV}>
+          <NotificationModalProvider>
+            <Router>
+              <ErrorHandler>
+                <AppPages />
+              </ErrorHandler>
+            </Router>
+          </NotificationModalProvider>
+        </Web3Provider>
+      </QueryClientProvider>
     </ErrorBoundary>
   );
 }
