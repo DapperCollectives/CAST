02308323af149873bd9bd2062c62fba9081872f4
German Urrustarazu <germanurrus@gmail.com>
2022-07-07 14:57:27 -0300

    add queries and comment code

--- frontend/packages/client/src/hooks/useJoinCommunity.js
+++ frontend/packages/client/src/hooks/useJoinCommunity.js
@@ -6,104 +6,120 @@ export default function useJoinCommunity() {
   const queryClient = useQueryClient();
   const { notifyError } = useErrorHandlerContext();
 
-  const { mutateAsync: createCommunityUserMutation } = useMutation(
-    async ({ communityId, user, injectedProvider }) => {
-      const { addr } = user;
-      const { currentUser } = injectedProvider;
-      const { signUserMessage } = currentUser();
-      const timestamp = Date.now().toString();
-      const hexTime = Buffer.from(timestamp).toString('hex');
-      const url = `${process.env.REACT_APP_BACK_END_SERVER_API}/communities/${communityId}/users`;
-      const _compositeSignatures = await signUserMessage(hexTime);
-      if (_compositeSignatures.indexOf('Declined:') > -1) {
-        return { success: false };
-      }
-      const compositeSignatures = getCompositeSigs(_compositeSignatures);
-      if (!compositeSignatures) {
-        return { error: 'No valid user signature found.' };
-      }
+  const { mutateAsync: createCommunityUserMutation, mutate: mutateOne } =
+    useMutation(
+      async ({ communityId, user, injectedProvider }) => {
+        const { addr } = user;
+        const { currentUser } = injectedProvider;
+        const { signUserMessage } = currentUser();
+        const timestamp = Date.now().toString();
+        const hexTime = Buffer.from(timestamp).toString('hex');
+        const url = `${process.env.REACT_APP_BACK_END_SERVER_API}/communities/${communityId}/users`;
+        const _compositeSignatures = await signUserMessage(hexTime);
+        if (_compositeSignatures.indexOf('Declined:') > -1) {
+          return { success: false };
+        }
+        const compositeSignatures = getCompositeSigs(_compositeSignatures);
+        if (!compositeSignatures) {
+          return { error: 'No valid user signature found.' };
+        }
 
-      try {
-        const fetchOptions = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            communityId: parseInt(communityId),
-            addr,
-            userType: 'member',
-            signingAddr: addr,
-            timestamp,
-            compositeSignatures,
-          }),
-        };
+        try {
+          const fetchOptions = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              communityId: parseInt(communityId),
+              addr,
+              userType: 'member',
+              signingAddr: addr,
+              timestamp,
+              compositeSignatures,
+            }),
+          };
 
-        const response = await fetch(url, fetchOptions);
-        const json = await response.json();
-        return { success: true, data: json };
-      } catch (err) {
-        notifyError(err, url);
+          const response = await fetch(url, fetchOptions);
+          const json = await response.json();
+          return { success: true, data: json };
+        } catch (err) {
+          notifyError(err, url);
+        }
       }
-    },
-    {
-      onSuccess: () =>
-        queryClient.invalidateQueries('communities-for-homepage'),
-    }
-  );
+      // {
+      //   onSuccess: () =>
+      //     queryClient.invalidateQueries('communities-for-homepage'),
+      // }
+    );
 
-  const { mutateAsync: deleteUserFromCommunityMutation } = useMutation(
-    async ({ communityId, user, injectedProvider }) => {
-      const { addr } = user;
-      const { currentUser } = injectedProvider;
-      const { signUserMessage } = currentUser();
-      const url = `${process.env.REACT_APP_BACK_END_SERVER_API}/communities/${communityId}/users/${addr}/member`;
-      const timestamp = Date.now().toString();
-      const hexTime = Buffer.from(timestamp).toString('hex');
-      const _compositeSignatures = await signUserMessage(hexTime);
-      if (_compositeSignatures.indexOf('Declined:') > -1) {
-        return { success: false };
-      }
-      const compositeSignatures = getCompositeSigs(_compositeSignatures);
-      if (!compositeSignatures) {
-        return { error: 'No valid user signature found.' };
-      }
+  const { mutateAsync: deleteUserFromCommunityMutation, mutate: mutateTwo } =
+    useMutation(
+      async ({ communityId, user, injectedProvider }) => {
+        const { addr } = user;
+        const { currentUser } = injectedProvider;
+        const { signUserMessage } = currentUser();
+        const url = `${process.env.REACT_APP_BACK_END_SERVER_API}/communities/${communityId}/users/${addr}/member`;
+        const timestamp = Date.now().toString();
+        const hexTime = Buffer.from(timestamp).toString('hex');
+        const _compositeSignatures = await signUserMessage(hexTime);
+        if (_compositeSignatures.indexOf('Declined:') > -1) {
+          return { success: false };
+        }
+        const compositeSignatures = getCompositeSigs(_compositeSignatures);
+        if (!compositeSignatures) {
+          return { error: 'No valid user signature found.' };
+        }
 
-      try {
-        const fetchOptions = {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            communityId: parseInt(communityId),
-            addr,
-            userType: 'member',
-            signingAddr: addr,
-            timestamp,
-            compositeSignatures,
-          }),
-        };
+        try {
+          const fetchOptions = {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              communityId: parseInt(communityId),
+              addr,
+              userType: 'member',
+              signingAddr: addr,
+              timestamp,
+              compositeSignatures,
+            }),
+          };
 
-        const response = await fetch(url, fetchOptions);
-        const json = await response.json();
-        return { success: true, data: json };
-      } catch (err) {
-        notifyError(err, url);
+          const response = await fetch(url, fetchOptions);
+          const json = await response.json();
+          return { success: true, data: json };
+        } catch (err) {
+          notifyError(err, url);
+        }
       }
-    },
-    {
-      onSuccess: () =>
-        queryClient.invalidateQueries('communities-for-homepage'),
-    }
-  );
+      // {
+      //   onSuccess: async (data, variables, context) => {
+      //  console.log('data', data, 'variables', variables, 'context', context);
+      //   const {
+      //     user: { addr },
+      //   } = variables;
+
+      // await queryClient.invalidateQueries('communities-for-homepage');
+      // await queryClient.invalidateQueries([
+      //   'connected-user-communities',
+      //   addr,
+      // ]);
+      //   },
+      // }
+    );
 
   const createCommunityUser = async (props) => {
-    return createCommunityUserMutation(props);
+    // return createCommunityUserMutation(props);
+    await mutateOne(props);
+    return { success: true };
   };
 
   const deleteUserFromCommunity = async (props) => {
-    return deleteUserFromCommunityMutation(props);
+    // return deleteUserFromCommunityMutation(props);
+    await mutateTwo(props);
+    return { success: true };
   };
 
   return {
