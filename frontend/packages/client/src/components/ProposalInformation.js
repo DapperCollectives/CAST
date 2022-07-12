cf21648903e34e4b71546317231a64a47c106142
Chris Keller <chris.keller@dapperlabs.com>
2022-07-12 15:48:40 -0500

    cas-134 display proposal strategy name instead of icon

--- frontend/packages/client/src/components/ProposalInformation.js
+++ frontend/packages/client/src/components/ProposalInformation.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Blockies from 'react-blockies';
-import { LinkOut, StrategyIcon } from './Svg';
+import { LinkOut } from './Svg';
 import { parseDateFromServer } from '../utils';
 import { useVotingResults, useWindowDimensions } from '../hooks';
 import useMediaQuery, { mediaMatchers } from '../hooks/useMediaQuery';
@@ -249,13 +249,13 @@ const ProposalInformation = ({
         >
           <p className="mb-5">Information</p>
           <InfoBlock
-            title={'Stategie(s)'}
+            title={strategies.length === 1 ? 'Strategy' : 'Strategies'}
             content={
               <div className="is-flex flex-1" onClick={openStrategyModal}>
                 {strategies.map((st, index) => {
                   return (
                     <div className="pr-2" key={`${st}-${index}`}>
-                      <StrategyIcon className="cursor-pointer" />
+                      {st.name}
                     </div>
                   );
                 })}
