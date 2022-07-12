ce1e3bcb68e2b065a6807b8b7a5b4180911fd048
Jonathan <jon.bluks@gmail.com>
2022-07-12 14:42:56 -0500

    CAS-177: Add pageSize to fetchLeaderboard

--- frontend/packages/client/src/api/leaderboard.js
+++ frontend/packages/client/src/api/leaderboard.js
@@ -1,13 +1,13 @@
 import { checkResponse } from '../utils';
 import { COMMUNITIES_URL } from './constants';
 
-const LEADERBOARD_SIZE = 10;
-const getLeaderBoardUrl = (communityId) =>
-  `${COMMUNITIES_URL}/${communityId}/leaderboard?count=${LEADERBOARD_SIZE}`;
+const DEFAULT_PAGE_SIZE = 10;
+const getLeaderBoardUrl = (communityId, pageSize) =>
+  `${COMMUNITIES_URL}/${communityId}/leaderboard?count=${pageSize}`;
 
-const fetchLeaderBoard = async (communityId) => {
+const fetchLeaderBoard = async (communityId, pageSize = DEFAULT_PAGE_SIZE) => {
   try {
-    const response = await fetch(getLeaderBoardUrl(communityId));
+    const response = await fetch(getLeaderBoardUrl(communityId, pageSize));
     return await checkResponse(response);
   } catch (err) {
     throw err;
