a72441c8cd49a7db51f3076f122b1b44b26b4114
German Urrustarazu <germanurrus@gmail.com>
2022-07-13 11:02:35 -0300

    remove duplicated migration files

--- backend/migrations/000021_add_achievements_details_column.down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE user_achievements RENAME TO community_users_achievements;
-ALTER TABLE community_users_achievements DROP COLUMN details;
-
--- backend/migrations/000021_add_achievements_details_column.up.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE community_users_achievements ADD details VARCHAR NOT NULL;
-ALTER TABLE community_users_achievements ADD UNIQUE (details);
-ALTER TABLE community_users_achievements RENAME TO user_achievements;
