341727ee5ef6870e47e193b352c9a764b0cef936
German Urrustarazu <germanurrus@gmail.com>
2022-07-14 12:08:44 -0300

    adjust styles

--- frontend/packages/client/src/components/Community/CommunityCard.js
+++ frontend/packages/client/src/components/Community/CommunityCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import JoinCommunityButton from './JoinCommunityButton';
-import WrapperResponsive from 'components/WrapperResponsive';
 import Blockies from 'react-blockies';
+import { useMediaQuery } from 'hooks';
 /**
  * CommunityCard will group communities on a row bases,
  * will use elementsPerRow to determine how many communities to render per row
@@ -10,22 +10,37 @@ import Blockies from 'react-blockies';
 const CommunityCard = ({ logo, name, body, id, slug }) => {
   const descriptionStyle = {
     lineHeight: '1.5em',
-    height: '3em',
+    maxHeight: '3rem',
     overflow: 'hidden',
   };
+
+  const isNotMobile = useMediaQuery();
+  const avatarSize = isNotMobile
+    ? {
+        logo: { width: 96, height: 96 },
+        blockie: { size: 10, scale: 9.6 },
+        columnStyle: { maxHeight: '120px' },
+      }
+    : {
+        logo: { width: 48, height: 48 },
+        blockie: { size: 10, scale: 4.8 },
+        columnStyle: { maxHeight: '72px' },
+      };
 
   return (
     <>
       <Link to={`/community/${id}?tab=proposals`} style={{ color: 'inherit' }}>
-        <div className="is-flex is-flex-grow-1 rounded-sm border-light p-5 is-flex-direction-column transition-all community-card">
+        <div className="is-flex is-flex-grow-1 rounded-sm border-light p-5 p-3-mobile is-flex-direction-column transition-all community-card">
           <div className="columns is-multiline is-flex-grow-1 is-mobile">
-            <div className="column is-narrow" style={{ maxHeight: '120px' }}>
+            <div
+              className="column is-narrow pr-2-mobile"
+              style={avatarSize.columnStyle}
+            >
               {logo ? (
                 <div
                   className="border-light rounded-sm"
                   style={{
-                    width: 96,
-                    height: 96,
+                    ...avatarSize.logo,
                     backgroundImage: `url(${logo})`,
                     backgroundSize: 'cover',
                   }}
@@ -33,25 +48,30 @@ const CommunityCard = ({ logo, name, body, id, slug }) => {
               ) : (
                 <Blockies
                   seed={slug ?? `seed-${id}`}
-                  size={10}
-                  scale={9.6}
+                  size={avatarSize.blockie.size}
+                  scale={avatarSize.blockie.scale}
                   className="rounded-sm"
                 />
               )}
             </div>
-            <div className="column is-flex is-flex-direction-column is-justify-content-center">
-              <WrapperResponsive
-                classNames="is-size-5 mb-2"
-                extraClasses="is-4 pt-1"
-                extraClassesMobile="is-6 pt-2"
-              >
+            <div
+              className="column pl-2-mobile pb-0-mobile is-flex is-flex-direction-column"
+              style={
+                isNotMobile
+                  ? {
+                      justifyContent: 'center',
+                    }
+                  : {}
+              }
+            >
+              <div className="is-size-5 is-size-6-mobile mb-2 is-4 is-6-mobile pt-0-mobile">
                 {name}
-              </WrapperResponsive>
-              <p className="has-text-grey" style={descriptionStyle}>
+              </div>
+              <p className="has-text-grey small-text" style={descriptionStyle}>
                 {body}
               </p>
             </div>
-            <div className="column is-narrow is-flex is-flex-direction-column is-justify-content-start">
+            <div className="column is-12-mobile p-0-mobile is-narrow-tablet is-flex is-flex-direction-column is-justify-content-start">
               <JoinCommunityButton communityId={id} darkMode={false} />
             </div>
           </div>
