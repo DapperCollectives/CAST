b74f710f1f73423aec1e533c8492a955279a5f9f
mannynotfound <manny@art404.com>
2022-07-14 10:48:52 -0400

    community page layout improvements

---
+++ frontend/packages/client/src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Title = ({ children }) => (
+  <h6 className="small-text has-text-weight-bold is-uppercase mb-1">
+    {children}
+  </h6>
+);
+
+export default Title;
