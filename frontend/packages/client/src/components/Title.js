414663d56d3b8540165039642449fa29537faefd
Manny404 <manny@art404.com>
2022-07-18 13:05:44 -0400

    initialize className with default empty string

--- frontend/packages/client/src/components/Title.js
+++ frontend/packages/client/src/components/Title.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Title = ({ children, className }) => (
+const Title = ({ children, className = '' }) => (
   <h6
     className={`small-text has-text-weight-bold is-uppercase mb-1 ${className}`}
   >
