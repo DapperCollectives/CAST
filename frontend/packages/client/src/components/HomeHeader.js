6980e1af2fc9fc965f432bce735bf3a6b49d205f
German Urrustarazu <germanurrus@gmail.com>
2022-07-22 17:53:41 -0300

    remove variable

--- frontend/packages/client/src/components/HomeHeader.js
+++ frontend/packages/client/src/components/HomeHeader.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const startPositions = [{}];
 export default function HomeHeader() {
   return (
     <div className="is-flex is-flex-direction-column is-align-items-center is-justify-content-center pb-7">
