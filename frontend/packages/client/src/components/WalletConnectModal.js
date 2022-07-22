df41bc721de97fd419328958141755a4b0430970
German Urrustarazu <germanurrus@gmail.com>
2022-07-22 20:44:54 -0300

    add foooter

--- frontend/packages/client/src/components/WalletConnectModal.js
+++ frontend/packages/client/src/components/WalletConnectModal.js
@@ -23,13 +23,13 @@ export default function WalletConnectModal({
   return (
     <div className={modalClasses}>
       <div className="modal-background"></div>
-      <div className="modal-card" style={{ maxWidth: '300px' }}>
+      <div className="modal-card" style={{ maxWidth: '375px' }}>
         <header
-          className="modal-card-head has-background-white columns is-mobile m-0 px-4 pt-4"
+          className="modal-card-head has-background-white columns is-mobile m-0 px-5 pt-4"
           style={{ borderBottom: 'none' }}
         >
           <div className="column p-0 is-flex flex-1">
-            <h2 className="is-size-4">Connect a Wallet</h2>
+            <h2 className="is-size-4">Connect a wallet</h2>
           </div>
           <div
             className={`column is-narrow px-0 has-text-right is-size-2 leading-tight cursor-pointer`}
@@ -39,27 +39,45 @@ export default function WalletConnectModal({
           </div>
         </header>
         <section
-          className="modal-card-body py-0 px-4"
+          className="modal-card-body py-0 px-5"
           style={{ minHeight: '280px' }}
         >
           {listOfServices.map((service) => {
             return (
               <div
                 className="border-light rounded-sm is-flex is-flex-direction-column is-justify-content-center mb-4 py-4 px-3 cursor-pointer strategy-selector transition-all"
-                style={{ minHeight: '48px' }}
+                style={{ height: '50px' }}
                 onClick={service.connectToService}
               >
                 <div className="columns">
-                  <div className="column">
-                    <img src={service.icon} alt={service.name} />
+                  <div className="column is-narrow is-flex is-align-items-center">
+                    <img
+                      src={service.icon}
+                      alt={service.name}
+                      style={{ width: '25px', height: '25px' }}
+                    />
                   </div>
-                  <div className="column">{service.name}</div>
+                  <div className="column is-flex is-align-items-center">
+                    <p className="is-size-5">{service.name}</p>
+                  </div>
                 </div>
               </div>
             );
           })}
         </section>
-        <footer className="modal-card-foot has-background-white pb-6"></footer>
+        <footer
+          className="modal-card-foot has-background-grey-lighter pb-6"
+          style={{ height: '107px' }}
+        >
+          <div className="is-flex">
+            <div className="columns">
+              <div className="column is-flex is-align-items-center">
+                <p className="is-size-5">Need a wallet? Learn more</p>
+              </div>
+            </div>
+          </div>
+          <div className="is-flex"></div>
+        </footer>
       </div>
     </div>
   );
