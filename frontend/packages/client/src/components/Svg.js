539feeb4ebad4aad6d7cdc82524f6aa08b5a8dd1
German Urrustarazu <germanurrus@gmail.com>
2022-07-24 19:47:38 -0300

    add close icon

--- frontend/packages/client/src/components/Svg.js
+++ frontend/packages/client/src/components/Svg.js
@@ -680,3 +680,30 @@ export const InvalidCheckMark = () => (
     </defs>
   </svg>
 );
+
+export const Close = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M21 21L3 3"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M21 3L2.99994 21.0001"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
