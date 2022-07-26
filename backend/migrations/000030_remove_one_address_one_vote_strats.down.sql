d540d43dac5721292a98107d229b22efcd730783
jacksonfoley <jackson.foley@dapperlabs.com>
2022-07-26 11:50:43 -0600

    fixes migrations so migrating up/down should work smoothly, and production migration will not fail

--- backend/migrations/000030_remove_one_address_one_vote_strats.down.sql
+++
@@ -1,1 +0,0 @@
--- no going back!
--- backend/migrations/000031_remove_one_address_one_vote_strats.up.sql
+++ backend/migrations/000031_remove_one_address_one_vote_strats.up.sql
@@ -1,5 +1,5 @@
-DELETE FROM voting_strategies WHERE key='one-address-one-vote-nft';
 DELETE FROM voting_strategies WHERE key='one-address-one-vote-ft';
+DELETE FROM voting_strategies WHERE key='one-address-one-vote-nft';
 DELETE FROM voting_strategies WHERE key='one-address-one-vote';
 
 -- Remove values from enum
