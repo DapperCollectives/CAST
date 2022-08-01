ed65a122960fd883e983b98b044e20e1637b57c8
German Urrustarazu <germanurrus@gmail.com>
2022-08-01 10:21:42 -0300

    temp updates

--- frontend/packages/client/src/components/Community/CommunityEditorLinks.js
+++ frontend/packages/client/src/components/Community/CommunityEditorLinks.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Website, Instagram, Twitter, Discord, Github } from 'components/Svg';
 import { WrapperResponsive, Loader } from 'components';
 import useLinkValidator from './hooks/useLinkValidator';
+import { useForm } from 'react-hook-form';
 
 const FormFieldsConfig = [
   {
@@ -102,7 +103,86 @@ export const CommunityLinksForm = ({
     </WrapperResponsive>
   );
 };
+const CommunityLinksForm2 = ({
+  formFields = FormFieldsConfig,
+  submitComponent,
+  onChangeHandler,
+  fields,
+  isUpdating = false,
+  wrapperMargin = 'mb-6',
+  wrapperMarginMobile = 'mb-4',
+  handleSubmit,
+}) => {
+  return (
+    <WrapperResponsive
+      classNames="border-light rounded-lg columns is-flex-direction-column is-mobile m-0"
+      extraClasses={`p-6 ${wrapperMargin}`}
+      extraClassesMobile={`p-4 ${wrapperMarginMobile}`}
+    >
+      <div className="columns flex-1">
+        <div className="column">
+          <div className="is-flex flex-1">
+            <WrapperResponsive
+              tag="h5"
+              classNames="title is-6 mb-2"
+              extraClassesMobile="mt-4"
+            >
+              Community Links
+            </WrapperResponsive>
+          </div>
+          <div className="is-flex flex-1 mt-5">
+            <p className="has-text-grey small-text">
+              Let’s fill out your Community profile. These details will be
+              publicly available and will help people know what your community
+              is all about.
+            </p>
+          </div>
+        </div>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <>
+          {formFields.map((formField, index) => (
+            <div
+              style={{ position: 'relative' }}
+              className="is-flex is-align-items-center mt-4"
+              key={`form-field-${index}`}
+            >
+              <input
+                type="text"
+                name={formField.fieldName}
+                className="rounded-sm border-light py-3 pr-3 column is-full"
+                placeholder={formField?.placeholder}
+                value={fields[formField.fieldName]}
+                maxLength={200}
+                onChange={(event) =>
+                  onChangeHandler(formField.fieldName)(event.target.value)
+                }
+                style={{
+                  paddingLeft: '34px',
+                }}
+                disabled={isUpdating}
+              />
+              <div
+                className="pl-3"
+                style={{
+                  position: 'absolute',
+                  height: 18,
+                  opacity: 0.3,
+                }}
+              >
+                {formField.iconComponent}
+              </div>
+            </div>
+          ))}
+          {submitComponent}
+        </>
+      </form>
+    </WrapperResponsive>
+  );
+};
+
 export default function CommunityEditorLinks(props = {}) {
+  const onSubmit = (data) => console.log(data);
   const { updateCommunity, ...fields } = props;
   const {
     websiteUrl = '',
@@ -111,59 +191,69 @@ export default function CommunityEditorLinks(props = {}) {
     discordUrl = '',
     githubUrl = '',
   } = fields;
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      websiteUrl,
+      twitterUrl,
+      instagramUrl,
+      discordUrl,
+      githubUrl,
+    },
+  });
   const [isUpdating, setIsUpdating] = useState(false);
-  const [links, setLinks] = useState({
-    websiteUrl,
-    twitterUrl,
-    instagramUrl,
-    discordUrl,
-    githubUrl,
-  });
+  // const [links, setLinks] = useState({
+  //   websiteUrl,
+  //   twitterUrl,
+  //   instagramUrl,
+  //   discordUrl,
+  //   githubUrl,
+  // });
 
-  const saveData = async () => {
-    setIsUpdating(true);
-    const updatedKeys = Object.keys(links).filter(
-      (key) => links[key] !== (fields[key] ?? '')
-    );
+  // const saveData = async () => {
+  //   setIsUpdating(true);
+  //   const updatedKeys = Object.keys(links).filter(
+  //     (key) => links[key] !== (fields[key] ?? '')
+  //   );
 
-    const updatedFields = Object.assign(
-      {},
-      ...updatedKeys.map((key) => ({ [key]: links[key] }))
-    );
-    await updateCommunity(updatedFields);
-    setIsUpdating(false);
-  };
+  //   const updatedFields = Object.assign(
+  //     {},
+  //     ...updatedKeys.map((key) => ({ [key]: links[key] }))
+  //   );
+  //   await updateCommunity(updatedFields);
+  //   setIsUpdating(false);
+  // };
 
-  const { isValid, hasChangedFromOriginal } = useLinkValidator({
-    links,
-    initialValues: fields,
-  });
+  // const { isValid, hasChangedFromOriginal } = useLinkValidator({
+  //   links,
+  //   initialValues: fields,
+  // });
 
-  const enableSave = isValid && hasChangedFromOriginal;
+  // const enableSave = isValid && hasChangedFromOriginal;
 
-  const changeHandler = (field) => (value) =>
-    setLinks((state) => ({
-      ...state,
-      [field]: value,
-    }));
+  // const changeHandler = (field) => (value) =>
+  //   setLinks((state) => ({
+  //     ...state,
+  //     [field]: value,
+  //   }));
 
   return (
-    <CommunityLinksForm
-      submitComponent={
-        <button
-          style={{ height: 48, width: '100%' }}
-          className={`button vote-button transition-all is-flex has-background-yellow rounded-sm mt-5 is-uppercase is-${
-            enableSave && !isUpdating ? 'enabled' : 'disabled'
-          }`}
-          onClick={!enableSave ? () => {} : saveData}
-        >
-          {!isUpdating && <>Save</>}
-          {isUpdating && <Loader size={18} spacing="mx-button-loader" />}
-        </button>
-      }
-      onChangeHandler={changeHandler}
-      fields={links}
-      isUpdating={isUpdating}
+    <CommunityLinksForm2
+        // submitComponent={
+        //   <button
+        //     style={{ height: 48, width: '100%' }}
+        //     className={`button vote-button transition-all is-flex has-background-yellow rounded-sm mt-5 is-uppercase is-${
+        //       enableSave && !isUpdating ? 'enabled' : 'disabled'
+        //     }`}
+        //     onClick={!enableSave ? () => {} : saveData}
+        //   >
+        //     {!isUpdating && <>Save</>}
+        //     {isUpdating && <Loader size={18} spacing="mx-button-loader" />}
+        //   </button>
+        // }
+        // onChangeHandler={changeHandler}
+        // fields={links}
+        // isUpdating={isUpdating}
     />
   );
 }
