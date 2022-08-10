e7a1da2688671236c5f4e80cfc0e042dc255d542
German Urrustarazu <germanurrus@gmail.com>
2022-08-10 17:42:07 -0300

    update form

--- frontend/packages/client/src/components/Community/StrategyEditorModal/StrategyInformationForm.js
+++ frontend/packages/client/src/components/Community/StrategyEditorModal/StrategyInformationForm.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Input } from 'components';
+import { useForm } from 'react-hook-form';
+import { useWebContext } from 'contexts/Web3';
+import { ActionButton } from 'components';
+import Input from 'components/common/Input';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { getSchema } from './FormConfig';
 
 const staticPlaceholders = {
   addr: 'Contract Address',
@@ -13,11 +18,23 @@ const staticPlaceholders = {
 export default function StrategyInformationForm({
   formFields = [],
   formData = {},
-  setField = () => {},
-  actionButton = null,
+  onSubmit = () => {},
 } = {}) {
+  console.log('default values', formData);
+  const { isValidFlowAddress } = useWebContext();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(getSchema(formFields, isValidFlowAddress)),
+    defaultValues: {
+      ...formData,
+    },
+  });
+
+  const { isDirty, isSubmitting, errors, isValid } = formState;
+
+  console.log('errors', errors);
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="columns is-flex-direction-column is-mobile m-0">
         <div className="small-text" style={{ color: '#757575' }}>
           Need help finding this information?{' '}
@@ -34,12 +51,20 @@ export default function StrategyInformationForm({
             key={index}
             placeholder={staticPlaceholders[field]}
             name={field}
-            value={formData[field] ?? ''}
-            onChange={setField(field)}
+            register={register}
+            error={errors[field]}
+            disabled={isSubmitting}
+            classNames="rounded-sm border-light p-3 column is-full is-full-mobile mt-4"
           />
         ))}
       </div>
-      {actionButton}
-    </>
+      <ActionButton
+        type="submit"
+        label="done"
+        enabled={(isDirty || isValid) && !isSubmitting}
+        loading={isSubmitting}
+        classNames="mt-5 has-button-border-hover"
+      />
+    </form>
   );
 }
