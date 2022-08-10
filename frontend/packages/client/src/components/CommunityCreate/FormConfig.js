c44b70457a1e8099899869498e2420f3ef781142
German Urrustarazu <germanurrus@gmail.com>
2022-08-10 13:57:05 -0300

    add schema for step three

--- frontend/packages/client/src/components/CommunityCreate/FormConfig.js
+++ frontend/packages/client/src/components/CommunityCreate/FormConfig.js
@@ -1,3 +1,4 @@
+import { addresValidation } from 'components/Community/CommunityEditorDetails';
 import {
   LinksSchema,
   linksFieldsArray,
@@ -8,6 +9,7 @@ import {
   profileFieldsArray,
   profileInitialValues,
 } from 'components/Community/CommunityEditorProfile';
+import * as yup from 'yup';
 
 const StepOneSchema = LinksSchema.concat(ProfileSchema);
 
@@ -15,4 +17,79 @@ const StepOneFieldsArray = [...linksFieldsArray, ...profileFieldsArray];
 
 const initialValues = { ...linksInitialValues, ...profileInitialValues };
 
-export { StepOneSchema, StepOneFieldsArray, initialValues };
+yup.addMethod(yup.string, 'makeOtherFieldsRequired', function (message) {
+  return this.test('checkOtherFields', message, function (value, context) {
+    const { path, parent } = context;
+    const otherProps = Object.keys(parent).filter(
+      (key) => key !== path && key !== 'onlyAuthorsToSubmitProposals'
+    );
+    // if currrent field does not have data but other do throw error
+    // to let the user know that it needs to be completed or field must be empty
+    if (value === '' && otherProps.some((key) => parent[key] !== '')) {
+      return this.createError({
+        path: `${this.path}`,
+        message,
+      });
+    }
+    return true;
+  });
+});
+
+const StepThreeSchema = (isValidFlowAddress) =>
+  yup.object().shape({
+    onlyAuthorsToSubmitProposals: yup.boolean(),
+    contractAddress: yup
+      .string()
+      .makeOtherFieldsRequired(
+        'Please enter a contract address if other fields are not empty'
+      )
+      .when('onlyAuthorsToSubmitProposals', {
+        is: true,
+        then: addresValidation(yup.string(), isValidFlowAddress, true),
+        otherwise: addresValidation(yup.string(), isValidFlowAddress, false),
+      }),
+    contractName: yup
+      .string()
+      .makeOtherFieldsRequired(
+        'Please enter a contract name if other fields are not empty'
+      )
+      .max(150, 'The maximum length for contract name is 150 characters')
+      .when('onlyAuthorsToSubmitProposals', {
+        is: false,
+        then: yup.string().required('Please enter a contract name'),
+      }),
+    storagePath: yup
+      .string()
+      .makeOtherFieldsRequired(
+        'Please enter a storage path if other fields are not empty'
+      )
+      .max(150, 'The maximum length for storage path is 150 characters')
+      .when('onlyAuthorsToSubmitProposals', {
+        is: false,
+        then: yup.string().required('Please enter a storage path'),
+      }),
+    proposalThreshold: yup
+      .string()
+      .makeOtherFieldsRequired(
+        'Please enter a proposal threshold if other fields are not empty'
+      )
+      .matches(/(^[0-9]+$|^$)/, 'Proposal threshold must be a number')
+      .when('onlyAuthorsToSubmitProposals', {
+        is: false,
+        then: yup.string().required('Please enter a proposal threshold'),
+      }),
+  });
+
+const stepOne = {
+  Schema: StepOneSchema,
+  FieldsArray: StepOneFieldsArray,
+  initialValues,
+};
+
+const stepThree = {
+  Schema: StepThreeSchema,
+  FieldsArray: StepOneFieldsArray,
+  initialValues,
+};
+
+export { stepOne, stepThree };
