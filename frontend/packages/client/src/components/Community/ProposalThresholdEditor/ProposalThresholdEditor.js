f217ae28f5f4acd7e0da50f3406fd86e057164d8
German Urrustarazu <germanurrus@gmail.com>
2022-08-11 19:02:50 -0300

    extract into component

--- frontend/packages/client/src/components/Community/ProposalThresholdEditor/ProposalThresholdEditor.js
+++ frontend/packages/client/src/components/Community/ProposalThresholdEditor/ProposalThresholdEditor.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useWebContext } from 'contexts/Web3';
-import { ActionButton, WrapperResponsive } from 'components';
-import Checkbox from 'components/common/Checkbox';
-import Input from 'components/common/Input';
+import { ActionButton } from 'components';
 import { yupResolver } from '@hookform/resolvers/yup';
 import isEqual from 'lodash/isEqual';
 import networks from 'networks';
 import { Schema } from './FormConfig';
+import ThresholdForm from './ThresholdForm';
 
 const networkConfig = networks[process.env.REACT_APP_FLOW_ENV];
 
@@ -119,77 +118,23 @@ export default function ProposalThresholdEditor({
   const { isDirty, isSubmitting, errors } = formState;
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <WrapperResponsive
-        classNames="border-light rounded-lg columns is-flex-direction-column is-mobile m-0"
-        extraClasses="p-6 mb-5"
-        extraClassesMobile="p-4 mb-4"
-      >
-        <div className="columns is-multiline">
+    <ThresholdForm
+      handleSubmit={handleSubmit(onSubmit)}
+      errors={errors}
+      register={register}
+      isSubmitting={isSubmitting}
+      submitComponent={
+        <div className="columns mb-5">
           <div className="column is-12">
-            <h4 className="has-text-weight-bold is-size-5">
-              Proposal Threshold
-            </h4>
-          </div>
-          <div className="column is-12">
-            <p className="small-text has-text-grey">
-              Proposal threshold is the minimum number of tokens community
-              members must hold in order to create a proposal.
-            </p>
+            <ActionButton
+              type="submit"
+              label="Next: VOTING STRATEGIES"
+              enabled={isDirty && !isSubmitting}
+              classNames="vote-button transition-all has-background-yellow mt-5"
+            />
           </div>
         </div>
-        <Input
-          placeholder="Contract Address"
-          register={register}
-          name="contractAddress"
-          disabled={isSubmitting}
-          error={errors['contractAddress']}
-          classNames="rounded-sm border-light p-3 column is-full is-full-mobile mt-4"
-        />
-        <Input
-          placeholder="Contract Name"
-          register={register}
-          name="contractName"
-          disabled={isSubmitting}
-          error={errors['contractName']}
-          classNames="rounded-sm border-light p-3 column is-full is-full-mobile mt-4"
-        />
-        <Input
-          placeholder="Collection Public Path"
-          name="storagePath"
-          register={register}
-          disabled={isSubmitting}
-          error={errors['storagePath']}
-          classNames="rounded-sm border-light p-3 column is-full is-full-mobile mt-4"
-        />
-        <Input
-          placeholder="Number of Tokens"
-          name="proposalThreshold"
-          register={register}
-          disabled={isSubmitting}
-          error={errors['proposalThreshold']}
-          classNames="rounded-sm border-light p-3 column is-full is-full-mobile mt-4"
-        />
-        <Checkbox
-          type="checkbox"
-          name="onlyAuthorsToSubmitProposals"
-          register={register}
-          disabled={isSubmitting}
-          error={errors['onlyAuthorsToSubmitProposals']}
-          label="Allow only designated authors to submit proposals"
-          labelClassNames="has-text-grey small-text"
-        />
-      </WrapperResponsive>
-      <div className="columns mb-5">
-        <div className="column is-12">
-          <ActionButton
-            type="submit"
-            label="Next: VOTING STRATEGIES"
-            enabled={isDirty && !isSubmitting}
-            classNames="vote-button transition-all has-background-yellow mt-5"
-          />
-        </div>
-      </div>
-    </form>
+      }
+    />
   );
 }
