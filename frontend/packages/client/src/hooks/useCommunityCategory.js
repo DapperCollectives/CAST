2e5ab75c18436cb04748affd350554061d9e58b4
German Urrustarazu <germanurrus@gmail.com>
2022-08-12 12:49:45 -0300

    use react quert

--- frontend/packages/client/src/hooks/useCommunityCategory.js
+++ frontend/packages/client/src/hooks/useCommunityCategory.js
@@ -1,34 +1,23 @@
-import { useCallback, useEffect, useReducer } from 'react';
+import { useQuery } from 'react-query';
 import { useErrorHandlerContext } from 'contexts/ErrorHandler';
-import { checkResponse } from 'utils';
-import { INITIAL_STATE, defaultReducer } from '../reducers';
+import { fetchCommunityCategories } from 'api/communityCategory';
 
 export default function useCommunityCategory() {
-  const [state, dispatch] = useReducer(defaultReducer, INITIAL_STATE);
   const { notifyError } = useErrorHandlerContext();
-  const getCommunityCategories = useCallback(async () => {
-    dispatch({ type: 'PROCESSING' });
-    const url = `${process.env.REACT_APP_BACK_END_SERVER_API}/community-categories`;
-    try {
-      const response = await fetch(url);
-      const communityCategory = await checkResponse(response);
-      dispatch({
-        type: 'SUCCESS',
-        payload: communityCategory,
-      });
-    } catch (err) {
-      // notify user of error
-      notifyError(err, url);
-      dispatch({ type: 'ERROR', payload: { errorData: err.message } });
-    }
-  }, [dispatch, notifyError]);
 
-  useEffect(() => {
-    getCommunityCategories();
-  }, [getCommunityCategories]);
+  const { isLoading, isError, data, error } = useQuery(
+    ['community-categories'],
+    async () => fetchCommunityCategories()
+  );
+
+  if (isError) {
+    notifyError(error, 'community-categories');
+  }
 
   return {
-    ...state,
-    getCommunityCategories,
+    isLoading,
+    isError,
+    error,
+    data,
   };
 }
