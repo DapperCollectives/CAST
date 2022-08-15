483a405b93cec3c81642732f8adb68f41ccdbec9
German Urrustarazu <germanurrus@gmail.com>
2022-08-15 17:13:21 -0300

    fix import

--- frontend/packages/client/src/components/Community/CommunityEditorDetails/index.js
+++ frontend/packages/client/src/components/Community/CommunityEditorDetails/index.js
@@ -1,6 +1,3 @@
-export {
-  default as CommunityEditorDetails,
-  CommunityUsersForm,
-} from './CommunityEditorDetails';
+export { default as CommunityEditorDetails } from './CommunityEditorDetails';
 export { default as AddressForm } from './AddressForm';
 export { AddressSchema, addresValidation } from './FormConfig';
