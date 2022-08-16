385bedcbe48253eb4282f1ee947a094dd01b3216
German Urrustarazu <germanurrus@gmail.com>
2022-08-16 14:19:03 -0300

    update hook

--- frontend/packages/client/src/hooks/useCommunityDetails.js
+++ frontend/packages/client/src/hooks/useCommunityDetails.js
@@ -1,89 +1,25 @@
-import { useCallback, useEffect, useReducer } from 'react';
 import { useErrorHandlerContext } from 'contexts/ErrorHandler';
-import { useWebContext } from 'contexts/Web3';
-import { checkResponse, getCompositeSigs } from 'utils';
-import { INITIAL_STATE, defaultReducer } from 'reducers';
+import { useQuery } from '@tanstack/react-query';
+import { fetchCommunityDetails } from 'api/communityDetails';
 
 export default function useCommunityDetails(id) {
-  const {
-    user: { addr },
-    injectedProvider,
-  } = useWebContext();
   const { notifyError } = useErrorHandlerContext();
-  const [state, dispatch] = useReducer(defaultReducer, INITIAL_STATE);
-  const getCommunityDetails = useCallback(async () => {
-    dispatch({ type: 'PROCESSING' });
-    const url = `${process.env.REACT_APP_BACK_END_SERVER_API}/communities/${id}`;
-    try {
-      const response = await fetch(url);
-      const details = await checkResponse(response);
-      // merging with mock data
-      const detailsUpdated = {
-        about: {
-          textAbout: details.body,
-        },
-        ...details,
-      };
 
-      dispatch({ type: 'SUCCESS', payload: detailsUpdated });
-    } catch (err) {
-      notifyError(err, url);
-      dispatch({ type: 'ERROR', payload: { errorData: err.message } });
+  const { isLoading, isError, data, error } = useQuery(
+    ['community-details', id],
+    async () => fetchCommunityDetails(id),
+    {
+      enabled: !!id,
+      onError: (error) => {
+        notifyError(error);
+      },
     }
-  }, [dispatch, id, notifyError]);
-
-  useEffect(() => {
-    getCommunityDetails();
-  }, [getCommunityDetails]);
-
-  const updateCommunityDetails = useCallback(
-    async (communityId, update) => {
-      const url = `${process.env.REACT_APP_BACK_END_SERVER_API}/communities/${communityId}`;
-      try {
-        const timestamp = Date.now().toString();
-        const hexTime = Buffer.from(timestamp).toString('hex');
-        const _compositeSignatures = await injectedProvider
-          .currentUser()
-          .signUserMessage(hexTime);
-
-        const compositeSignatures = getCompositeSigs(_compositeSignatures);
-
-        if (!compositeSignatures) {
-          return { error: 'No valid user signature found.' };
-        }
-
-        const fetchOptions = {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            ...update,
-            signingAddr: addr,
-            timestamp,
-            compositeSignatures,
-          }),
-        };
-        dispatch({ type: 'PROCESSING' });
-        const response = await fetch(url, fetchOptions);
-        const json = await checkResponse(response);
-        dispatch({
-          type: 'SUCCESS',
-          payload: json,
-        });
-        return json;
-      } catch (err) {
-        notifyError(err, url);
-        dispatch({ type: 'ERROR', payload: { errorData: err.message } });
-        return { error: err.message };
-      }
-    },
-    [dispatch, notifyError, addr, injectedProvider]
   );
 
   return {
-    ...state,
-    updateCommunityDetails,
-    getCommunityDetails,
+    isLoading,
+    isError,
+    data,
+    error,
   };
 }
