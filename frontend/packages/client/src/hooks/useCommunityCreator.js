505a2f5b13306ffa9c711a42d4485ee569fc2a9e
German Urrustarazu <germanurrus@gmail.com>
2022-08-16 12:21:19 -0300

    add onError callback

--- frontend/packages/client/src/hooks/useCommunityCreator.js
+++ frontend/packages/client/src/hooks/useCommunityCreator.js
@@ -16,59 +16,63 @@ export default function useCommunityCreator() {
     isSuccess,
     data,
     error,
-  } = useMutation(async ({ injectedProvider, communityData }) => {
-    const timestamp = Date.now().toString();
-    const hexTime = Buffer.from(timestamp).toString('hex');
-    const _compositeSignatures = await injectedProvider
-      .currentUser()
-      .signUserMessage(hexTime);
+  } = useMutation(
+    async ({ injectedProvider, communityData }) => {
+      const timestamp = Date.now().toString();
+      const hexTime = Buffer.from(timestamp).toString('hex');
+      const _compositeSignatures = await injectedProvider
+        .currentUser()
+        .signUserMessage(hexTime);
 
-    const compositeSignatures = getCompositeSigs(_compositeSignatures);
+      const compositeSignatures = getCompositeSigs(_compositeSignatures);
 
-    if (!compositeSignatures) {
-      const statusText = 'No valid user signature found.';
-      // modal error will open
-      notifyError({
-        status: 'Something went wrong with creating the community.',
-        statusText,
+      if (!compositeSignatures) {
+        const statusText = 'No valid user signature found.';
+        // modal error will open
+        notifyError({
+          status: 'Something went wrong with creating the community.',
+          statusText,
+        });
+        return;
+      }
+
+      const { logo, banner, ...otherProps } = communityData;
+
+      // check for logo / banner uploads
+      // admins can edit later the images
+      let communityLogo;
+      let communityBanner;
+      if (logo?.file) {
+        try {
+          communityLogo = await uploadFile(logo.file);
+        } catch (err) {
+          communityLogo = undefined;
+        }
+      }
+      if (banner?.file) {
+        try {
+          communityBanner = await uploadFile(banner.file);
+        } catch (err) {
+          communityBanner = undefined;
+        }
+      }
+      return createCommunityApiReq({
+        payload: {
+          ...otherProps,
+          logo: communityLogo?.fileUrl,
+          bannerImgUrl: communityBanner?.fileUrl,
+        },
+        timestamp,
+        compositeSignatures,
       });
-      return;
-    }
-
-    const { logo, banner, ...otherProps } = communityData;
-
-    // check for logo / banner uploads
-    // admins can edit later the images
-    let communityLogo;
-    let communityBanner;
-    if (logo?.file) {
-      try {
-        communityLogo = await uploadFile(logo.file);
-      } catch (err) {
-        communityLogo = undefined;
-      }
-    }
-    if (banner?.file) {
-      try {
-        communityBanner = await uploadFile(banner.file);
-      } catch (err) {
-        communityBanner = undefined;
-      }
-    }
-    return createCommunityApiReq({
-      payload: {
-        ...otherProps,
-        logo: communityLogo?.fileUrl,
-        bannerImgUrl: communityBanner?.fileUrl,
+    },
+    {
+      onError: (error) => {
+        notifyError(error);
       },
-      timestamp,
-      compositeSignatures,
-    });
-  });
+    }
+  );
 
-  if (isError) {
-    notifyError(error);
-  }
   return {
     isLoading,
     isError,
