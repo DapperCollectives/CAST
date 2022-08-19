06bde04e3d53f7b7d28dac2a0968770744056e2a
Tuffon Benavides <benavides.tuffon@gmail.com>
2022-08-19 12:41:45 -0700

    Fixed a nil pointer reference when strategies array is omitted (#377)
    * Fixed a nil pointer reference when strategies array is omitted
    
    * Added another nil check for strategies array
    
    * added test to cover nil pointer regression

--- backend/main/server/controllers.go
+++ backend/main/server/controllers.go
@@ -322,10 +322,12 @@ func (a *App) createCommunity(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//Validate Contract Thresholds
-	err = validateContractThreshold(*payload.Strategies)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, err.Error())
-		return
+	if payload.Strategies != nil {
+		err = validateContractThreshold(*payload.Strategies)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, err.Error())
+			return
+		}
 	}
 
 	c, httpStatus, err := helpers.createCommunity(payload)
@@ -352,10 +354,12 @@ func (a *App) updateCommunity(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//Validate Contract Thresholds
-	err = validateContractThreshold(*payload.Strategies)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, err.Error())
-		return
+	if payload.Strategies != nil {
+		err = validateContractThreshold(*payload.Strategies)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, err.Error())
+			return
+		}
 	}
 
 	c, httpStatus, err := helpers.updateCommunity(id, payload)
