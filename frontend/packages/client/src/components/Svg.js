36ec12b25741dd2a2ecdcfbfc504fe47459cf338
German Urrustarazu <germanurrus@gmail.com>
2022-08-29 11:57:54 -0300

    update style

--- frontend/packages/client/src/components/Svg.js
+++ frontend/packages/client/src/components/Svg.js
@@ -681,11 +681,11 @@ export const InvalidCheckMark = () => (
   </svg>
 );
 
-export const Close = () => {
+export const Close = ({ width = '24', height = '24' } = {}) => {
   return (
     <svg
-      width="24"
-      height="24"
+      width={width}
+      height={height}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
