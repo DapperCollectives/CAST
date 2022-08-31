e222574ab2eb577a4fa43ec4af08e111a53f67df
German Urrustarazu <germanurrus@gmail.com>
2022-08-31 11:20:18 -0300

    update component

--- frontend/packages/client/src/components/common/CustomDatePicker.js
+++ frontend/packages/client/src/components/common/CustomDatePicker.js
@@ -2,6 +2,7 @@ import React from 'react';
 import DatePicker from 'react-datepicker';
 import { Controller } from 'react-hook-form';
 import FadeIn from 'components/FadeIn';
+import { Calendar } from 'components/Svg';
 
 export default function CustomDatePicker({
   control,
@@ -11,23 +12,50 @@ export default function CustomDatePicker({
   maxDate,
   disabled = false,
   placeholderText,
+  errorMessage,
 } = {}) {
   return (
-    <Controller
-      control={control}
-      name={fieldName}
-      render={({ field }) => (
-        <DatePicker
-          placeholderText={placeholderText}
-          selected={field.value}
-          minDate={minDate ?? new Date()}
-          maxDate={maxDate}
-          onFocus={(e) => !notMobile && e.target.blur()}
-          onChange={(date) => field.onChange(date)}
-          className="border-light rounded-sm column is-full is-full-mobile p-3"
-          disabled={disabled}
+    <div className="columns is-multiline p-0 m-0 is-flex-grow-1">
+      <div
+        className="p-0 pr-2 p-0-mobile mb-4-mobile m-0 column is-fullwidth"
+        style={{ position: 'relative' }}
+      >
+        <Controller
+          control={control}
+          name={fieldName}
+          render={({ field }) => (
+            <DatePicker
+              placeholderText={placeholderText}
+              selected={field.value}
+              minDate={minDate ?? new Date()}
+              maxDate={maxDate}
+              onFocus={(e) => !notMobile && e.target.blur()}
+              onChange={(date) => field.onChange(date)}
+              className="border-light rounded-sm column is-full is-full-mobile p-3"
+              disabled={disabled}
+            />
+          )}
         />
+        <div
+          style={{
+            position: 'absolute',
+            right: 15,
+            top: 7,
+            pointerEvents: 'none',
+          }}
+        >
+          <Calendar />
+        </div>
+      </div>
+      {errorMessage && (
+        <div className="column p-0 is-12">
+          <FadeIn>
+            <div className="pl-1 mt-2 mb-4">
+              <p className="smaller-text has-text-red">{errorMessage}</p>
+            </div>
+          </FadeIn>
+        </div>
       )}
-    />
+    </div>
   );
 }
