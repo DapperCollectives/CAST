e60b52632c2c3cacdc30f1b76822769425b1b079
German Urrustarazu <germanurrus@gmail.com>
2022-08-31 15:38:42 -0300

    set focus on address

--- frontend/packages/client/src/components/common/Form.js
+++ frontend/packages/client/src/components/common/Form.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Form = ({ removeInnerForm = false, children, onSubmit, formId } = {}) => {
+  // TODO: make enter to jump to next input field on form
+  const checkKeyDown = (e) => {
+    if (e.code === 'Enter') e.preventDefault();
+  };
   return removeInnerForm ? (
     <>{children}</>
   ) : (
-    <form onSubmit={onSubmit} id={formId}>
+    <form onSubmit={onSubmit} id={formId} onKeyDown={(e) => checkKeyDown(e)}>
       {children}
     </form>
   );
