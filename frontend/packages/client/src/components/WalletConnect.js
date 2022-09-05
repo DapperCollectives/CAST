c56be1e76f5502d5a268d241626b211dbaf47193
German Urrustarazu <germanurrus@gmail.com>
2022-09-05 19:20:16 -0300

    update component

--- frontend/packages/client/src/components/WalletConnect.js
+++ frontend/packages/client/src/components/WalletConnect.js
@@ -1,21 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import Blockies from 'react-blockies';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Web3Consumer } from '../contexts/Web3';
 import { Copy } from 'components/Svg';
+import { useMediaQuery, useOnClickOutside } from 'hooks';
+import { truncateAddress } from 'utils';
+import classnames from 'classnames';
+import Tooltip from './Tooltip';
 
 const SignInOutButton = ({
   user: { loggedIn, addr },
   openWalletModal,
   injectedProvider,
   closeModal,
+  expandContainer = false,
 }) => {
+  const notMobile = useMediaQuery();
+
   const [dropDownClass, setDropDownClass] = useState('');
+  const [addressCopied, setAddressCopied] = useState(false);
+
+  const dropdownRef = useRef();
+
+  const closeDropdown = (e) => {
+    setDropDownClass('');
+  };
+
+  useOnClickOutside(dropdownRef, closeDropdown);
 
   const signOut = (event) => {
     event.preventDefault();
     event.stopPropagation();
     injectedProvider.unauthenticate();
-    // setDropDownClass('');
+    setDropDownClass('');
   };
 
   const connectWallet = (event) => {
@@ -33,31 +50,69 @@ const SignInOutButton = ({
     setDropDownClass('is-active');
   };
 
-  const closeOnBlur = (e) => {
-    console.log(e);
-    if (e.type === 'blur') {
-      setDropDownClass('');
+  const markAddressCopied = () => setAddressCopied(true);
+
+  useEffect(() => {
+    let timeout;
+    if (addressCopied) {
+      timeout = setTimeout(() => {
+        setAddressCopied(false);
+      }, 500);
     }
-  };
+    return () => clearTimeout(timeout);
+  }, [addressCopied]);
+
+  const dropdownBackground = classnames('', {
+    'wallet-connect-background': dropDownClass && !notMobile,
+  });
+  const buttonClass = classnames(
+    'wallet-connect button is-uppercase transition-all small-text rounded-sm',
+    { 'is-primary': !loggedIn },
+    { 'px-2': !notMobile }
+  );
+  const addressStyle = classnames('', { 'smaller-text': !notMobile });
+  const containerAddressStyle = classnames('is-flex', { 'pl-2': !notMobile });
+
+  const containerButtonStyle = classnames('is-flex is-align-items-center', {
+    'flex-1 is-justify-content-space-between': notMobile,
+  });
 
   return (
-    <div onBlur={closeOnBlur}>
+    <>
+      <div className={dropdownBackground} />
       <div
         className={`dropdown ${dropDownClass}`}
         aria-haspopup="true"
         aria-controls="dropdown-menu"
+        style={{ position: 'relative' }}
       >
-        <div className="dropdown-trigger">
+        <div
+          className="dropdown-trigger is-flex is-justify-content-flex-end"
+          style={notMobile && expandContainer ? { width: '280px' } : {}}
+        >
           <button
             onClick={loggedIn ? openDropdown : connectWallet}
-            className="wallet-connect button is-primary is-uppercase transition-all small-text"
+            className={buttonClass}
+            style={
+              notMobile
+                ? {
+                    ...(loggedIn ? { width: '147px' } : { width: '206px' }),
+                    height: '40px',
+                  }
+                : { width: '119px', height: '31px' }
+            }
           >
             {loggedIn ? (
-              <div>
-                <span className="is-hidden-mobile is-hidden-connect">
-                  {addr} -&nbsp;
-                </span>
-                <span>Disconnect</span>
+              <div className={containerButtonStyle}>
+                <Blockies
+                  seed={addr}
+                  size={notMobile ? 6.5 : 5}
+                  scale={4}
+                  className="blockies"
+                />
+                <div className={containerAddressStyle}>
+                  <p className={addressStyle}>{truncateAddress(addr, 4, 4)}</p>
+                </div>
               </div>
             ) : (
               <>
@@ -67,42 +122,68 @@ const SignInOutButton = ({
             )}
           </button>
         </div>
-        <div className="dropdown-menu" id="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="is-flex">
-              <div className="columns">
-                <div className="column">{addr}</div>
-                <div className="column">
-                  <CopyToClipboard
-                    text={addr}
-                    onCopy={(e) => {
-                      console.log('eee', e);
-                    }}
-                  >
-                    <div className="button is-white rounded-sm">
-                      <Copy />
-                    </div>
-                  </CopyToClipboard>
-                </div>
-              </div>
-            </div>
-            <hr className="dropdown-divider" />
 
-            <div
-              className="button is-fullwidth rounded-sm is-uppercase is-flex small-text has-text-white has-background-black"
-              style={{ minHeight: '32px' }}
-              onClick={signOut}
-            >
-              disconnect
+        <div
+          className="dropdown-menu wallet-connect-content"
+          id="dropdown-menu"
+          role="menu"
+          ref={dropdownRef}
+          style={!notMobile ? { left: '-160px' } : {}}
+        >
+          <div className="dropdown-content p-0" style={{ width: '277px' }}>
+            <div className="px-4 pt-4 pb-2">
+              <Tooltip
+                classNames="is-flex is-flex-grow-1 is-align-items-center transition-all"
+                position="top"
+                text="Copied!"
+                alwaysVisible={true}
+                enabled={addressCopied}
+              >
+                <CopyToClipboard text={addr} onCopy={markAddressCopied}>
+                  <div
+                    className="columns flex-1 is-mobile m-0 px-4 py-0 rounded-sm button is-white border-light"
+                    style={{ borderColor: 'hsl(0deg, 0%, 86%)' }}
+                  >
+                    <div
+                      className="column p-0 is-flex is-align-items-center"
+                      style={{ height: '32px' }}
+                    >
+                      <span className="small-text">{addr}</span>
+                    </div>
+                    <div
+                      className="column p-0 is-flex is-align-items-center is-narrow"
+                      style={{ height: '32px' }}
+                    >
+                      <div
+                        className="is-flex is-align-items-center py-0 px-1"
+                        style={{ height: '23px' }}
+                      >
+                        <Copy />
+                      </div>
+                    </div>
+                  </div>
+                </CopyToClipboard>
+              </Tooltip>
+            </div>
+
+            <hr className="dropdown-divider" />
+            <div className="px-4 pb-4 pt-2">
+              <div
+                className="button is-fullwidth rounded-sm is-uppercase is-flex small-text has-text-white has-background-black"
+                style={{ minHeight: '32px' }}
+                onClick={signOut}
+              >
+                disconnect
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
-const CurrentUser = ({ web3, closeModal }) => {
+const CurrentUser = ({ web3, closeModal, expandContainer }) => {
   const { user, injectedProvider, openWalletModal } = web3;
   if (!user) {
     return null;
@@ -115,6 +196,7 @@ const CurrentUser = ({ web3, closeModal }) => {
         injectedProvider={injectedProvider}
         openWalletModal={openWalletModal}
         closeModal={closeModal}
+        expandContainer={expandContainer}
       />
     </div>
   );
