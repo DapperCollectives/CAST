9a3688aea5a081599887472d71daa61b3d01b6cc
Manny404 <manny@art404.com>
2022-09-08 12:56:33 -0400

    community card + join button updates (#431)

--- frontend/packages/client/src/components/Svg.js
+++ frontend/packages/client/src/components/Svg.js
@@ -757,3 +757,49 @@ export const Copy = () => (
     />
   </svg>
 );
+
+export const Eye = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+    className="eye"
+  >
+    <path
+      fill="#fff"
+      stroke="#171717"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M12 18c5.523 0 10-6 10-6s-4.477-6-10-6-10 6-10 6 4.477 6 10 6z"
+    />
+    <path
+      stroke="#171717"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M12 14a2 2 0 100-4 2 2 0 000 4z"
+    />
+  </svg>
+);
+
+export const HideEye = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+    className="hide-eye"
+  >
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M4 4l16 16M16.5 16.756C15.147 17.485 13.619 18 12 18c-5.523 0-10-6-10-6s2.088-2.799 5.172-4.573M19.5 14.634C21.056 13.265 22 12 22 12s-4.477-6-10-6c-.338 0-.671.022-1 .064M13.323 13.5a2 2 0 01-2.762-2.889"
+    />
+  </svg>
+);
