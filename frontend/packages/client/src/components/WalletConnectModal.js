1b5d865f1baf7d44d75a6ae4a9af9b850a3577c2
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2022-09-13 20:10:17 -0700

    Revert "Fix provider icon on wallet list"
    This reverts commit f57c563596a3e8349617cdd6beb26dd8a7cde7fd.

--- frontend/packages/client/src/components/WalletConnectModal.js
+++ frontend/packages/client/src/components/WalletConnectModal.js
@@ -5,6 +5,13 @@ import classnames from 'classnames';
 import sortBy from 'lodash/sortBy';
 import { ArrowRight, Close } from './Svg';
 
+const getWalletIcon = (provider) => {
+  if (provider?.name === 'Lilico') {
+    return 'https://raw.githubusercontent.com/Outblock/Lilico-Web/main/asset/logo-dis.png';
+  }
+  return `https://fcl-discovery.onflow.org${provider.icon}`;
+};
+
 export default function WalletConnectModal({
   services = [],
   openModal,
@@ -33,7 +40,7 @@ export default function WalletConnectModal({
             );
             closeModal();
           },
-          icon: service.provider.icon,
+          icon: getWalletIcon(service.provider),
           name: service.provider.name,
         })),
         (service) => service.name
