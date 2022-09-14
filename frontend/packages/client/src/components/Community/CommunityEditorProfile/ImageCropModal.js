6b086f13cbcf72a51b9bdfe1ec9bdc418dfc6c4e
German Urrustarazu <germanurrus@gmail.com>
2022-09-14 16:24:49 -0300

    add component

--- frontend/packages/client/src/components/Community/CommunityEditorProfile/ImageCropModal.js
+++ frontend/packages/client/src/components/Community/CommunityEditorProfile/ImageCropModal.js
@@ -71,6 +71,9 @@ export default function ImageCropperModal({
         >
           <div className="columns is-multiline p-0 m-0">
             <div className="column is-flex is-justify-content-center is-align-items-center is-12 p-0 m-0 ">
+              <div>
+                <Svg name="RemoveOutline" />
+              </div>
               <input
                 type="range"
                 min={1}
@@ -82,6 +85,9 @@ export default function ImageCropperModal({
                   setZoom(e.target.value);
                 }}
               />
+              <div>
+                <Svg name="PlusOutline" />
+              </div>
             </div>
             <div className="column m-0 p-0 is-flex is-12 is-align-iterms-center is-justify-content-center">
               <div className="is-flex flex-1 px-5">
