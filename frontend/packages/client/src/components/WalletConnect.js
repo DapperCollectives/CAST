c409f3faabda8c9954691f09fd1d5901a686d4ad
German Urrustarazú <germanurrus@users.noreply.github.com>
2022-09-14 13:30:35 -0300

    CAS 150 - fix connect wallet dropdown style (#454)
    * fix style
    
    * update left

--- frontend/packages/client/src/components/WalletConnect.js
+++ frontend/packages/client/src/components/WalletConnect.js
@@ -91,7 +91,7 @@ const SignInOutButton = ({
                     ...(loggedIn ? { width: '147px' } : { width: '206px' }),
                     height: '40px',
                   }
-                : { width: '119px', height: '32px' }
+                : { width: '105px', height: '32px' }
             }
           >
             {loggedIn ? (
@@ -120,7 +120,7 @@ const SignInOutButton = ({
           id="dropdown-menu"
           role="menu"
           ref={dropdownRef}
-          style={!notMobile ? { left: '-160px' } : { left: '-130px' }}
+          style={!notMobile ? { left: '-170px' } : { left: '-130px' }}
         >
           <div
             className="dropdown-content p-0 rounded"
