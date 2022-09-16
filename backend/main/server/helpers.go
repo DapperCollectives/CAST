c70d9ff8b51931b3373c1e2261187f4527fb2fbc
Jonathan Bluks <jonathan.bluks@dapperlabs.com>
2022-09-16 10:11:05 -0700

    CAS-502: Update return value for helpers with error and status

--- backend/main/server/helpers.go
+++ backend/main/server/helpers.go
@@ -28,6 +28,11 @@ type Helpers struct {
 	A *App
 }
 
+type errorHandler struct {
+	err error
+	status int
+}
+
 func (h *Helpers) Initialize(app *App) {
 	h.A = app
 }
@@ -434,23 +439,23 @@ func (h *Helpers) validateVote(p models.Proposal, v models.Vote) error {
 	return nil
 }
 
-func (h *Helpers) fetchCommunity(id int) (models.Community, int, error) {
+func (h *Helpers) fetchCommunity(id int) (models.Community, errorHandler) {
 	community := models.Community{ID: id}
 
 	if err := community.GetCommunity(h.A.DB); err != nil {
 		log.Error().Err(err)
 		switch err.Error() {
 		case pgx.ErrNoRows.Error():
-			return models.Community{}, http.StatusNotFound, errors.New("Community not found.")
+			return models.Community{}, errorHandler { err: errors.New("Community not found."), status: http.StatusNotFound }
 		default:
-			return models.Community{}, http.StatusInternalServerError, err
+			return models.Community{}, errorHandler { err: err, status: http.StatusInternalServerError }
 		}
 	}
 
-	return community, http.StatusOK, nil
+	return community, errorHandler { err: nil, status: http.StatusOK }
 }
 
-func (h *Helpers) searchCommuntities(query string) (interface{}, error) {
+func (h *Helpers) searchCommunities(query string) (interface{}, error) {
 	results, err := models.SearchForCommunity(h.A.DB, query)
 	if err != nil {
 		return []models.Community{}, err
@@ -459,27 +464,27 @@ func (h *Helpers) searchCommuntities(query string) (interface{}, error) {
 	return results, nil
 }
 
-func (h *Helpers) createProposal(p models.Proposal) (models.Proposal, int, error) {
+func (h *Helpers) createProposal(p models.Proposal) (models.Proposal, errorHandler) {
 	if p.Voucher != nil {
 		if err := h.validateUserViaVoucher(p.Creator_addr, p.Voucher); err != nil {
-			return models.Proposal{}, http.StatusForbidden, err
+			return models.Proposal{}, errorHandler{ err: err, status: http.StatusForbidden}
 		}
 	} else {
 		if err := h.validateUser(p.Creator_addr, p.Timestamp, p.Composite_signatures); err != nil {
-			return models.Proposal{}, http.StatusForbidden, err
+			return models.Proposal{}, errorHandler{ err: err, status: http.StatusForbidden}
 		}
 	}
 
-	community, httpStatus, err := h.fetchCommunity(p.Community_id)
-	if err != nil {
-		return models.Proposal{}, httpStatus, err
+	community, e := h.fetchCommunity(p.Community_id)
+	if e.err != nil {
+		return models.Proposal{}, e
 	}
 
 	strategy, err := models.MatchStrategyByProposal(*community.Strategies, *p.Strategy)
 	if err != nil {
 		errMsg := "Community does not have this strategy available."
 		log.Error().Err(err).Msg(errMsg)
-		return models.Proposal{}, http.StatusInternalServerError, errors.New(errMsg)
+		return models.Proposal{}, errorHandler{ err: errors.New(errMsg), status: http.StatusInternalServerError }
 
 	}
 
@@ -492,31 +497,31 @@ func (h *Helpers) createProposal(p models.Proposal) (models.Proposal, int, error
 	}
 
 	if err := h.snapshot(&strategy, &p); err != nil {
-		return models.Proposal{}, http.StatusInternalServerError, err
+		return models.Proposal{}, errorHandler { err: err, status: http.StatusInternalServerError }
 	}
 
 	if err := h.enforceCommunityRestrictions(community, p, strategy); err != nil {
-		return models.Proposal{}, http.StatusForbidden, err
+		return models.Proposal{}, errorHandler { err: err, status: http.StatusForbidden }
 	}
 
 	if err := h.processSnapshotStatus(&strategy, &p); err != nil {
 		errMsg := "Error processing snapshot status."
 		log.Error().Err(err).Msg(errMsg)
-		return models.Proposal{}, http.StatusInternalServerError, errors.New(errMsg)
+		return models.Proposal{}, errorHandler { err: errors.New(errMsg), status: http.StatusInternalServerError }
 	}
 
 	p.Cid, err = h.pinJSONToIpfs(p)
 	if err != nil {
 		log.Error().Err(err).Msg("IPFS error: " + err.Error())
 		errMsg := "Error pinning JSON to IPFS."
-		return models.Proposal{}, http.StatusInternalServerError, errors.New(errMsg)
+		return models.Proposal{}, errorHandler { err: errors.New(errMsg), status: http.StatusInternalServerError }
 	}
 
 	validate := validator.New()
 	vErr := validate.Struct(p)
 	if vErr != nil {
 		log.Error().Err(vErr)
-		return models.Proposal{}, http.StatusBadRequest, errors.New("Invalid proposal.")
+		return models.Proposal{}, errorHandler { err: errors.New("Invalid proposal."), status: http.StatusBadRequest }
 	}
 
 	if os.Getenv("APP_ENV") == "PRODUCTION" {
@@ -526,10 +531,10 @@ func (h *Helpers) createProposal(p models.Proposal) (models.Proposal, int, error
 	}
 
 	if err := p.CreateProposal(h.A.DB); err != nil {
-		return models.Proposal{}, http.StatusInternalServerError, err
+		return models.Proposal{}, errorHandler { err: err, status: http.StatusInternalServerError }
 	}
 
-	return p, http.StatusCreated, nil
+	return p, errorHandler { err: nil, status: http.StatusCreated }
 }
 
 func (h *Helpers) enforceCommunityRestrictions(
@@ -669,42 +674,42 @@ func (h *Helpers) processCommunityRoles(
 	return nil
 }
 
-func (h *Helpers) updateCommunity(id int, payload models.UpdateCommunityRequestPayload) (models.Community, int, error) {
-	c, httpStatus, err := h.fetchCommunity(id)
-	if err != nil {
-		return models.Community{}, httpStatus, err
+func (h *Helpers) updateCommunity(id int, payload models.UpdateCommunityRequestPayload) (models.Community, errorHandler) {
+	c, e := h.fetchCommunity(id)
+	if e.err != nil {
+		return models.Community{}, e
 	}
 
 	// validate is community creator
 	// TODO: update to validating address is admin
 	if err := c.CanUpdateCommunity(h.A.DB, payload.Signing_addr); err != nil {
 		log.Error().Err(err)
-		return models.Community{}, http.StatusForbidden, err
+		return models.Community{}, errorHandler { err: err, status: http.StatusForbidden }
 	}
 
 	if payload.Voucher != nil {
 		if err := h.validateUserViaVoucher(payload.Signing_addr, payload.Voucher); err != nil {
 			log.Error().Err(err)
-			return models.Community{}, http.StatusForbidden, err
+			return models.Community{}, errorHandler { err: err, status: http.StatusForbidden }
 		}
 	} else {
 		if err := h.validateUser(payload.Signing_addr, payload.Timestamp, payload.Composite_signatures); err != nil {
 			log.Error().Err(err)
-			return models.Community{}, http.StatusForbidden, err
+			return models.Community{}, errorHandler { err: err, status: http.StatusForbidden }
 		}
 	}
 
 	if err := c.UpdateCommunity(h.A.DB, &payload); err != nil {
 		log.Error().Err(err)
-		return models.Community{}, http.StatusInternalServerError, err
+		return models.Community{}, errorHandler { err: err, status: http.StatusInternalServerError }
 	}
 
-	c, httpStatus, err = h.fetchCommunity(id)
-	if err != nil {
-		return models.Community{}, httpStatus, err
+	c, e = h.fetchCommunity(id)
+	if e.err != nil {
+		return models.Community{}, e
 	}
 
-	return c, http.StatusOK, nil
+	return c, errorHandler { err: nil, status: http.StatusOK }
 }
 
 func (h *Helpers) removeUserRole(payload models.CommunityUserPayload) (int, error) {
