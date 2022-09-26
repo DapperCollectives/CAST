a5142e24619d816462747d7ce7cd49932561d203
Richard Melkonian <r.v.melkonian@gmail.com>
2022-09-26 12:55:39 +0100

    added pagination for search endpoint

--- backend/main/server/controllers.go
+++ backend/main/server/controllers.go
@@ -304,8 +304,13 @@ func (a *App) searchCommunities(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 	}
+	pageParams := getPageParams(*r, 25)
 
-	respondWithJSON(w, http.StatusOK, results)
+	totalRecords := results.(int)
+	pageParams.TotalRecords = totalRecords
+
+	response := shared.GetPaginatedResponseWithPayload(results, pageParams)
+	respondWithJSON(w, http.StatusOK, response)
 }
 
 func (a *App) getCommunity(w http.ResponseWriter, r *http.Request) {
