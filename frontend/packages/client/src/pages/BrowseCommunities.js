7e273f155407b88a09c198b6a74302e067a901f1
German Urrustarazu <germanurrus@gmail.com>
2022-09-26 15:08:47 -0300

    update style

--- frontend/packages/client/src/pages/BrowseCommunities.js
+++ frontend/packages/client/src/pages/BrowseCommunities.js
@@ -1,7 +1,7 @@
 export default function BrowseCommunities() {
   return (
-    <section className="is-flex is-align-items-center is-justify-content-center full-height">
-      <div>Browse Communities</div>
+    <section className="is-flex is-align-items-flex-start is-justify-content-center full-height">
+      <div className="is-flex">Search</div>
     </section>
   );
 }
