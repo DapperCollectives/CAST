60dcd940bd684f635dc1e924bc88e032f34b67af
German Urrustarazu <germanurrus@gmail.com>
2022-09-27 12:48:15 -0300

    extend pill component

--- frontend/packages/client/src/components/Pill.js
+++ frontend/packages/client/src/components/Pill.js
@@ -1,18 +1,33 @@
 import classnames from 'classnames';
 
 export default function Pill({
-  status,
+  text,
   classNames,
   backgroundColorClass,
   textColorClass = 'has-text-white',
   outlined = false,
+  padding = 'px-2 py-1',
+  fontSize = 'smallest-text',
+  fontWeight = 'has-text-weight-bold',
+  onClick,
 }) {
   const className = classnames(
-    'px-2 py-1 rounded-lg smallest-text has-text-weight-bold',
+    'rounded-lg',
     { [textColorClass]: !!textColorClass },
     { [backgroundColorClass]: !!backgroundColorClass },
-    { [classNames]: !!classNames }
+    { [padding]: !!padding },
+    { [fontSize]: !!fontSize },
+    { [fontWeight]: !!fontWeight },
+    { [classNames]: !!classNames },
+    { button: !!onClick }
   );
 
-  return <span className={className}>{status}</span>;
+  if (onClick) {
+    return (
+      <div className="is-flex" onClick={onClick}>
+        <span className={className}>{text}</span>
+      </div>
+    );
+  }
+  return <span className={className}>{text}</span>;
 }
