1a8a4565ef13354c868b829907b4ad0da89192cc
German Urrustarazu <germanurrus@gmail.com>
2022-09-28 12:28:11 -0300

    use fake data

--- frontend/packages/client/src/hooks/useCommunitySearch.js
+++ frontend/packages/client/src/hooks/useCommunitySearch.js
@@ -1,11 +1,75 @@
 import { useErrorHandlerContext } from 'contexts/ErrorHandler';
-import { checkResponse, getPagination, getPlainData } from 'utils';
+import { checkResponse, getPagination, wait } from 'utils';
 import { useInfiniteQuery } from '@tanstack/react-query';
+import queryString from 'query-string';
 import { PAGINATION_INITIAL_STATE } from 'reducers';
+
+const mockedData = [
+  {
+    id: 1,
+    name: 'Flow',
+    body: 'Vote on Flow Validators',
+    logo: 'https://gateway.pinata.cloud/ipfs/QmZBsiJPi6ZWiqPjcftVCYqdgU81i2oBrNmw3XXE1Q3zg7',
+    category: 'dao',
+  },
+  {
+    id: 8,
+    name: 'Dapper Collectives',
+    category: 'dao',
+    logo: 'https://gateway.pinata.cloud/ipfs/QmZBsiJPi6ZWiqPjcftVCYqdgU81i2oBrNmw3XXE1Q3zg7',
+    body: 'Build better, together.',
+    slug: '8af17484ca',
+  },
+  {
+    id: 28,
+    name: 'test community',
+    category: 'dao',
+    logo: 'https://dappercollectives.mypinata.cloud/ipfs/QmZrVBCn8Ta7ALn93U5zGGexpLhN9JoSg1BSCjKwb9zjVj',
+
+    slug: 'bcd906ef4c',
+  },
+  {
+    id: 23,
+    name: 'erer',
+    category: 'dao',
+    logo: 'https://dappercollectives.mypinata.cloud/ipfs/QmZrVBCn8Ta7ALn93U5zGGexpLhN9JoSg1BSCjKwb9zjVj',
+    body: 'erer',
+  },
+  {
+    id: 39,
+    name: 'CommunityD',
+    category: 'dao',
+    slug: '96fae5cb46',
+  },
+  {
+    id: 68,
+    name: 'test for voting proposals',
+    category: 'dao',
+    body: 'ss',
+    slug: '5980ce5a08',
+    contractName: 'FlowToken',
+  },
+  {
+    id: 32,
+    name: 'Community For Testing - updated',
+    category: 'dao',
+    logo: 'https://dappercollectives.mypinata.cloud/ipfs/QmPrki3jEHoYfrKQd3FWZcBvHUGXYc2R16yuPmTW2ULUxU',
+  },
+];
+
+export const getPlainDataCustom = (data) => {
+  const mergeddata = data?.pages?.reduce(
+    (prev, current) =>
+      current.data.results ? [...prev, ...current.data.results] : prev,
+    []
+  );
+  return { filters: data?.pages?.[0].data?.filters, results: mergeddata };
+};
 
 export default function useCommunitySearch({
   count: countParam = PAGINATION_INITIAL_STATE.count,
   searchText,
+  filters,
 } = {}) {
   const initialPageParam = [0, countParam, 0, -1];
 
@@ -14,6 +78,7 @@ export default function useCommunitySearch({
   const queryUniqueKey = [
     'search-community',
     encodeURIComponent(String(searchText)),
+    filters,
   ];
 
   const { isLoading, isError, data, error, fetchNextPage } = useInfiniteQuery(
@@ -22,13 +87,28 @@ export default function useCommunitySearch({
       const [start, count] = pageParam;
       const searchText = queryKey[1];
       const filters = queryKey[2];
-      const url = `${process.env.REACT_APP_BACK_END_SERVER_API}/communities/search/${searchText}?filters=${filters}count=${count}&start=${start}`;
+
+      console.log(queryKey);
+      const queryParams = queryString.stringify({
+        filters,
+        start,
+        count,
+      });
+
+      console.log(queryParams);
+
+      const url = `${process.env.REACT_APP_BACK_END_SERVER_API}/communities/search/${searchText}?${queryParams}`;
 
       // use \default when search is for all
       const response = await fetch(url);
-      // return checkResponse(response);
+      const resp = await checkResponse(response);
+
+      await wait(2000);
       return {
+        ...resp,
         data: {
+          // results: resp.data,
+          results: mockedData,
           filters: [
             { text: 'All', amount: 22 },
             { text: 'DAO', amount: 3 },
@@ -36,62 +116,6 @@ export default function useCommunitySearch({
             { text: 'NFT', amount: 8 },
             { text: 'Collector', amount: 0 },
           ],
-          results: [
-            {
-              id: 1,
-              name: 'Flow',
-              body: 'Vote on Flow Validators',
-              logo: 'https://gateway.pinata.cloud/ipfs/QmZBsiJPi6ZWiqPjcftVCYqdgU81i2oBrNmw3XXE1Q3zg7',
-              category: 'dao',
-            },
-            {
-              id: 8,
-              name: 'Dapper Collectives',
-              category: 'dao',
-              logo: 'https://gateway.pinata.cloud/ipfs/QmZBsiJPi6ZWiqPjcftVCYqdgU81i2oBrNmw3XXE1Q3zg7',
-              body: 'Build better, together.',
-              slug: '8af17484ca',
-            },
-            {
-              id: 28,
-              name: 'test community',
-              category: 'dao',
-              logo: 'https://dappercollectives.mypinata.cloud/ipfs/QmZrVBCn8Ta7ALn93U5zGGexpLhN9JoSg1BSCjKwb9zjVj',
-
-              slug: 'bcd906ef4c',
-            },
-            {
-              id: 23,
-              name: 'erer',
-              category: 'dao',
-              logo: 'https://dappercollectives.mypinata.cloud/ipfs/QmZrVBCn8Ta7ALn93U5zGGexpLhN9JoSg1BSCjKwb9zjVj',
-              body: 'erer',
-            },
-            {
-              id: 39,
-              name: 'CommunityD',
-              category: 'dao',
-              slug: '96fae5cb46',
-            },
-            {
-              id: 68,
-              name: 'test for voting proposals',
-              category: 'dao',
-              body: 'ss',
-              slug: '5980ce5a08',
-              contractName: 'FlowToken',
-            },
-            {
-              id: 32,
-              name: 'Community For Testing - updated',
-              category: 'dao',
-              logo: 'https://dappercollectives.mypinata.cloud/ipfs/QmPrki3jEHoYfrKQd3FWZcBvHUGXYc2R16yuPmTW2ULUxU',
-            },
-          ],
-          start: 0,
-          count: 10,
-          totalRecords: 8,
-          next: -1,
         },
       };
     },
@@ -109,7 +133,7 @@ export default function useCommunitySearch({
   return {
     isLoading,
     isError,
-    data: getPlainData(data),
+    data: getPlainDataCustom(data),
     error,
     pagination: getPagination(data),
     fetchNextPage,
