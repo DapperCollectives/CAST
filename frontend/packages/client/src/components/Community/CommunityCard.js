5a69b3fd2975401efe681660d64651e4c6f1f1cf
German Urrustarazu <germanurrus@gmail.com>
2022-10-02 17:29:54 -0300

    update community card

--- frontend/packages/client/src/components/Community/CommunityCard.js
+++ frontend/packages/client/src/components/Community/CommunityCard.js
@@ -1,18 +1,45 @@
 import Blockies from 'react-blockies';
 import { Link } from 'react-router-dom';
 import { useMediaQuery } from 'hooks';
+import classNames from 'classnames';
 import JoinCommunityButton from './JoinCommunityButton';
 
 /**
  * CommunityCard will group communities on a row bases,
  * will use elementsPerRow to determine how many communities to render per row
  */
-const CommunityCard = ({ logo, name, body, id, slug, hideJoin }) => {
-  const descriptionStyle = {
-    lineHeight: '1.5em',
-    maxHeight: '3rem',
-  };
 
+const NameAndBody = ({ name, body, isMobile }) => {
+  const containerClass = classNames(
+    'column is-flex is-flex-direction-column is-justyfy-content-flex-start',
+    { 'is-12': isMobile }
+  );
+  const nameClass = classNames(
+    'is-size-4 is-size-5-mobile is-4 line-clamp-2 has-text-weight-bold',
+    { 'mb-2': !isMobile },
+    { 'mb-1': isMobile }
+  );
+  const bodyClass = classNames('has-text-grey line-clamp-2', {
+    'small-text': isMobile,
+  });
+
+  return (
+    <div className={containerClass}>
+      <div className={nameClass}>{name}</div>
+      <p
+        className={bodyClass}
+        style={{
+          lineHeight: '1.5em',
+          maxHeight: '3rem',
+        }}
+      >
+        {body}
+      </p>
+    </div>
+  );
+};
+
+const CommunityCard = ({ logo, name, body, id, slug, hideJoin }) => {
   const isNotMobile = useMediaQuery();
   const avatarSize = isNotMobile
     ? {
@@ -27,68 +54,50 @@ const CommunityCard = ({ logo, name, body, id, slug, hideJoin }) => {
       };
 
   return (
-    <>
-      <Link to={`/community/${id}?tab=proposals`} style={{ color: 'inherit' }}>
-        <div className="is-flex is-flex-grow-1 rounded-lg border-light p-5 p-3-mobile is-flex-direction-column transition-all community-card">
-          <div className="columns is-multiline is-flex-grow-1 is-mobile">
-            <div
-              className="column is-narrow pr-2-mobile"
-              style={avatarSize.columnStyle}
-            >
-              {logo ? (
-                <div
-                  className="border-light rounded-full"
-                  style={{
-                    ...avatarSize.logo,
-                    backgroundImage: `url(${logo})`,
-                    backgroundSize: 'cover',
-                  }}
-                />
-              ) : (
-                <Blockies
-                  seed={slug ?? `seed-${id}`}
-                  size={avatarSize.blockie.size}
-                  scale={avatarSize.blockie.scale}
-                  className="rounded-full"
-                />
-              )}
-            </div>
-            <div
-              className="column pl-2-mobile pb-0-mobile is-flex is-flex-direction-column"
-              style={
-                isNotMobile
-                  ? {
-                      justifyContent: 'center',
-                    }
-                  : {}
-              }
-            >
-              <div className="is-size-5 is-size-6-mobile mb-2 is-4 is-6-mobile pt-0-mobile line-clamp-2">
-                {name}
-              </div>
-              <p
-                className="has-text-grey small-text line-clamp-2"
-                style={descriptionStyle}
-              >
-                {body}
-              </p>
-            </div>
-            {!hideJoin && (
+    <Link to={`/community/${id}?tab=proposals`} style={{ color: 'inherit' }}>
+      <div className="is-flex is-flex-grow-1 rounded-lg border-light p-5 p-4-mobile is-flex-direction-column transition-all community-card">
+        <div className="columns is-multiline is-flex-grow-1 is-mobile">
+          <div
+            className="column is-narrow pr-2-mobile"
+            style={avatarSize.columnStyle}
+          >
+            {logo ? (
               <div
-                className="is-flex is-align-items-center mr-2"
-                style={{ maxWidth: 40 }}
-              >
-                <JoinCommunityButton
-                  communityId={id}
-                  hasTooltip={isNotMobile}
-                />
-              </div>
+                className="border-light rounded-full"
+                style={{
+                  ...avatarSize.logo,
+                  backgroundImage: `url(${logo})`,
+                  backgroundSize: 'cover',
+                }}
+              />
+            ) : (
+              <Blockies
+                seed={slug ?? `seed-${id}`}
+                size={avatarSize.blockie.size}
+                scale={avatarSize.blockie.scale}
+                className="rounded-full"
+              />
             )}
           </div>
+          {isNotMobile && <NameAndBody name={name} body={body} />}
+          {!hideJoin && (
+            <div
+              className={`column is-flex is-align-items-flex-start${
+                isNotMobile
+                  ? ' is-narrow'
+                  : ' flex-1 is-justify-content-flex-end'
+              } `}
+            >
+              <JoinCommunityButton communityId={id} />
+            </div>
+          )}
+          {!isNotMobile && <NameAndBody name={name} body={body} isMobile />}
         </div>
-      </Link>
-    </>
+      </div>
+    </Link>
   );
 };
+
+CommunityCard.displayName = 'Community-Card';
 
 export default CommunityCard;
