f56fc5631554d3fb82085ba19518547a949b7e2a
German Urrustarazu <germanurrus@gmail.com>
2022-10-04 17:08:06 -0300

    update styles

--- frontend/packages/client/src/components/FilterPill.js
+++ frontend/packages/client/src/components/FilterPill.js
@@ -8,16 +8,21 @@ export default function FilterPill({
   amount,
   borderColor,
 }) {
+  const isDisabled = Number(amount) === 0;
   const className = classnames(
     'rounded-lg mx-2 mt-2 px-4 py-2 button pill-border',
-    { 'has-text-black': !selected },
-    { 'has-text-white has-background-black': selected },
-    { [borderColor]: !!borderColor && !selected },
+    { 'has-text-black': !selected && !isDisabled },
+    { 'has-text-white has-background-black': selected && !isDisabled },
+    { [borderColor]: !!borderColor && !selected && !isDisabled },
+    { 'pill-border-disabled': isDisabled },
     { [classNames]: !!classNames }
   );
 
   return (
-    <div className="is-flex" onClick={() => onClick(text)}>
+    <div
+      className="is-flex"
+      onClick={!isDisabled ? () => onClick(text) : () => {}}
+    >
       <div className={className}>
         <p>
           <span className="has-text-weight-bold">{text}</span> {amount}
