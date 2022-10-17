900f21957d8b9f9c3bcc10aafea2f3a7d24f85e7
manny404 <manny@art404.com>
2022-10-17 11:53:56 -0400

    make Manage modal use Modal components

--- frontend/packages/client/src/components/modals/Notifications/Manage.js
+++ frontend/packages/client/src/components/modals/Notifications/Manage.js
@@ -1,40 +1,32 @@
 import { Link } from 'react-router-dom';
-import { Svg } from '@cast/shared-components';
+import Modal from '../Modal';
 
 const NotificationsManage = ({ onClose }) => (
-  <div className="has-background-white rounded-sm">
-    <header className="modal-card-head has-background-white m-0">
-      <div className="is-flex is-align-items-center flex-1">
-        <h2 className="is-size-4 is-capitalized">manage notifications</h2>
-      </div>
-      <div
-        className="cursor-pointer is-flex is-align-items-center"
-        onClick={onClose}
-      >
-        <Svg name="Close" />
-      </div>
-    </header>
-    <section className="modal-card-body p-4">
-      <p className="small-text has-text-grey">
-        Notification settings can be managed on the settings page
-      </p>
-      <div className="is-flex mt-4">
-        <button
-          className="button rounded-lg is-outlined px-3 flex-1 mr-2"
-          onClick={onClose}
-        >
-          <b>Close</b>
-        </button>
-        <Link
-          to="/settings"
-          className="button rounded-lg is-primary px-3 flex-1 mr-2"
-          onClick={onClose}
-        >
-          <b>Settings</b>
-        </Link>
-      </div>
-    </section>
-  </div>
+  <Modal>
+    <Modal.Header title="manage notifications" onClose={onClose} />
+    <Modal.BodyContainer>
+      <section className="modal-card-body">
+        <p className="small-text has-text-grey">
+          Notification settings can be managed on the settings page
+        </p>
+        <div className="is-flex mt-4">
+          <button
+            className="button rounded-lg is-outlined px-3 flex-1 mr-2"
+            onClick={onClose}
+          >
+            <b>Close</b>
+          </button>
+          <Link
+            to="/settings"
+            className="button rounded-lg is-primary px-3 flex-1 mr-2"
+            onClick={onClose}
+          >
+            <b>Settings</b>
+          </Link>
+        </div>
+      </section>
+    </Modal.BodyContainer>
+  </Modal>
 );
 
 export default NotificationsManage;
