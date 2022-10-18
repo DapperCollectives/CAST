c62d7decc8a235da52a4ceae8fb7af8426e37ff6
German Urrustarazu <germanurrus@gmail.com>
2022-10-18 15:39:59 -0300

    extract into component

---
+++ frontend/packages/client/src/components/ProposalCreate/StepOne/SingleVoteExample.js
@@ -0,0 +1,26 @@
+export default function SingleVoteExample() {
+  return (
+    <>
+      {['Option A', 'Option B', 'Option C'].map((text, index) => (
+        <div
+          className="is-flex is-align-items-center mb-1"
+          style={{ whiteSpace: 'nowrap' }}
+        >
+          <div
+            className="rounded-full has-background-grey has-text-white mr-2 is-flex is-align-items-center is-justify-content-center"
+            style={{ width: 12, height: 12 }}
+          ></div>
+          {index === 1 && (
+            <div
+              className="rounded-full has-background-grey has-text-white mr-2 is-flex is-align-items-center is-justify-content-center"
+              style={{ width: 12, height: 12 }}
+            >
+              <span style={{ fontSize: 8, paddingTop: 1 }}>&#x2713;</span>
+            </div>
+          )}
+          <span className="smaller-text">{text}</span>
+        </div>
+      ))}
+    </>
+  );
+}
