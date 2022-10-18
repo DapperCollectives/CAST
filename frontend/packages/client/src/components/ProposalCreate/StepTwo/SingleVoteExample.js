3cbe0af5875c63742a1e4f42e8cd5d8b355e9857
German Urrustarazu <germanurrus@gmail.com>
2022-10-18 16:13:35 -0300

    remove span and fix check

--- frontend/packages/client/src/components/ProposalCreate/StepTwo/SingleVoteExample.js
+++ frontend/packages/client/src/components/ProposalCreate/StepTwo/SingleVoteExample.js
@@ -7,17 +7,18 @@ export default function SingleVoteExample() {
           style={{ whiteSpace: 'nowrap' }}
           key={index}
         >
-          <div
-            className="rounded-full has-background-grey has-text-white mr-2 is-flex is-align-items-center is-justify-content-center"
-            style={{ width: 12, height: 12 }}
-          ></div>
-          {index === 1 && (
+          {index === 1 ? (
             <div
               className="rounded-full has-background-grey has-text-white mr-2 is-flex is-align-items-center is-justify-content-center"
               style={{ width: 12, height: 12 }}
             >
               <span style={{ fontSize: 8, paddingTop: 1 }}>&#x2713;</span>
             </div>
+          ) : (
+            <div
+              className="rounded-full has-background-grey has-text-white mr-2 is-flex is-align-items-center is-justify-content-center"
+              style={{ width: 12, height: 12 }}
+            />
           )}
           <span className="smaller-text">{text}</span>
         </div>
