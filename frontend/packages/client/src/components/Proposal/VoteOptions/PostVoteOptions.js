5632e293386b3cd2bbeceb057e0173557d24e9d4
manny404 <manny@art404.com>
2022-10-19 16:16:21 -0400

    make postvoteoptions responsive

--- frontend/packages/client/src/components/Proposal/VoteOptions/PostVoteOptions.js
+++ frontend/packages/client/src/components/Proposal/VoteOptions/PostVoteOptions.js
@@ -26,35 +26,50 @@ const PostVoteOptions = ({ communityId, proposalId }) => {
           You successfully voted on this proposal!
         </b>
       </div>
-      <div className="is-flex is-align-items-center mt-5">
-        <a
-          className="is-flex flex-1 is-align-items-center rounded-lg button twitter-button mr-2"
-          href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-            proposalUrl
-          )}`}
-          target="_blank"
-          rel="noreferrer noopenner"
-        >
-          <Svg name="Twitter" />
-          <span className="ml-1">Share on Twitter</span>
-        </a>
-        <CopyToClipboard
-          text={window?.location?.href}
-          onCopy={() => setLinkCopied(true)}
-        >
-          <div className="is-flex flex-1 is-align-items-center rounded-lg button mx-2">
-            <Svg name="Copy" />
-            <span className="ml-1">
-              {linkCopied ? 'Link Copied!' : 'Copy Link'}
-            </span>
-          </div>
-        </CopyToClipboard>
+      <div className="is-flex is-flex-wrap-wrap is-flex-direction-column is-flex-direction-row-desktop-only is-justify-content-center mt-5 columns is-mobile p-0">
         <div
-          className="is-flex flex-1 is-align-items-center rounded-lg button ml-2"
-          onClick={openNotificationsModal}
+          className="column is-full-mobile p-0 mr-2-desktop mb-2"
+          style={{ flexGrow: 0 }}
         >
-          <Svg name="Bell" />
-          <span className="ml-1">Subscribe for Updates</span>
+          <a
+            className="is-flex flex-1 is-align-items-center rounded-lg button twitter-button"
+            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+              proposalUrl
+            )}`}
+            target="_blank"
+            rel="noreferrer noopenner"
+          >
+            <Svg name="Twitter" />
+            <span className="ml-1">Share on Twitter</span>
+          </a>
+        </div>
+        <div
+          className="column is-full-mobile p-0 mx-2-desktop mb-2"
+          style={{ flexGrow: 0 }}
+        >
+          <CopyToClipboard
+            text={window?.location?.href}
+            onCopy={() => setLinkCopied(true)}
+          >
+            <div className="is-flex flex-1 is-align-items-center rounded-lg button">
+              <Svg name="Copy" />
+              <span className="ml-1">
+                {linkCopied ? 'Link Copied!' : 'Copy Link'}
+              </span>
+            </div>
+          </CopyToClipboard>
+        </div>
+        <div
+          className="column is-full-mobile p-0 ml-2-desktop mb-2"
+          style={{ flexGrow: 0 }}
+        >
+          <div
+            className="is-flex flex-1 is-align-items-center rounded-lg button"
+            onClick={openNotificationsModal}
+          >
+            <Svg name="Bell" />
+            <span className="ml-1">Subscribe for Updates</span>
+          </div>
         </div>
       </div>
     </div>
