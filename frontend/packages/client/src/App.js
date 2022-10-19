03269d7a13e678afe49dbd6c389d7a9bf02c8f0e
Manny404 <manny@art404.com>
2022-10-19 13:43:02 -0400

    VSL-245 - add chakra ui + toast component (#659)
    * add toast chakra-ui + toast component
    
    * add .env.development

--- frontend/packages/client/src/App.js
+++ frontend/packages/client/src/App.js
@@ -6,6 +6,7 @@ import NotificationModalProvider from 'contexts/NotificationModal';
 import { Web3Provider } from 'contexts/Web3';
 import { ErrorHandler } from 'components';
 import { IS_PRODUCTION } from 'const';
+import { ChakraProvider } from '@chakra-ui/react';
 import Hotjar from '@hotjar/browser';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -29,13 +30,15 @@ function App() {
     >
       <QueryClientProvider client={queryClient}>
         <Web3Provider network={process.env.REACT_APP_FLOW_ENV}>
-          <NotificationModalProvider>
-            <Router>
-              <ErrorHandler>
-                <AppPages />
-              </ErrorHandler>
-            </Router>
-          </NotificationModalProvider>
+          <Router>
+            <ChakraProvider>
+              <NotificationModalProvider>
+                <ErrorHandler>
+                  <AppPages />
+                </ErrorHandler>
+              </NotificationModalProvider>
+            </ChakraProvider>
+          </Router>
         </Web3Provider>
         {!IS_PRODUCTION && <ReactQueryDevtools initialIsOpen={false} />}
       </QueryClientProvider>
