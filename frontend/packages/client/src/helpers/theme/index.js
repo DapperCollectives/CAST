fd810353ffe685d2d59347e7aa5f54a575e0c300
German Urrustarazu <germanurrus@gmail.com>
2022-10-19 19:08:45 -0300

    updates

--- frontend/packages/client/src/helpers/theme/index.js
+++ frontend/packages/client/src/helpers/theme/index.js
@@ -3,8 +3,9 @@ import { extendTheme } from '@chakra-ui/react';
 const theme = extendTheme({
   styles: {
     global: (props) => ({
-      body: {
+      'html, body': {
         bg: 'transparent',
+        lineHeight: 'normal',
       },
     }),
   },
