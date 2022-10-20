55e2779a18b52f9428955cc17c257c863a280e7b
David Slone <dbslone@gmail.com>
2022-10-20 15:37:52 -0700

    add eslintignore

--- frontend/packages/client/src/index.js
+++ frontend/packages/client/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import './index.css';
 
 // hack for buffer error on react-scripts version > 5
+// eslint-disable-next-line
 window.Buffer = window.Buffer || require('buffer').Buffer;
 
 Sentry.init({
