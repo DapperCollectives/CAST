27f9750056073c78aa716d8120babe2e25bcf671
German Urrustarazu <germanurrus@gmail.com>
2022-10-25 17:02:21 -0300

    update cards

--- frontend/packages/client/src/components/ProposalCreate/WarningMessage/NFTCard.js
+++ frontend/packages/client/src/components/ProposalCreate/WarningMessage/NFTCard.js
@@ -1,17 +1,15 @@
-import { Svg } from '@cast/shared-components';
 import { Card } from 'components';
 import { Flex, Text } from '@chakra-ui/react';
 
-export default function FlowCard({ title, balance }) {
+export default function FlowCard({ title, balance, contractName }) {
   return (
     <Card variant="flowBox">
       <Text fontSize="xxs" fontWeight="bold" variant="allUpperCase">
         {title}
       </Text>
       <Flex alignItems="center" gap={2}>
-        <Svg name="Flow" />
         <Text size="lg" fontSize="lg" fontWeight="bold">
-          {balance} FLOW
+          {balance} {contractName} NFT
         </Text>
       </Flex>
     </Card>
