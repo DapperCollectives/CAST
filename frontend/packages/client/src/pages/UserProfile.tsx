661fb8754f7d98feb0a37ffa2cb244f54a251cb1
German Urrustarazu <germanurrus@gmail.com>
2022-10-31 13:15:03 -0300

    temp updates

--- frontend/packages/client/src/pages/UserProfile.tsx
+++ frontend/packages/client/src/pages/UserProfile.tsx
@@ -1,11 +1,74 @@
+import Blockies from 'react-blockies';
+import { useWebContext } from 'contexts/Web3';
+import { CommunityLinks } from 'components';
+import WalletAddress from 'components/WalletAddress';
+import { useMediaQuery } from 'hooks';
+import { Box, Flex, Spacer } from '@chakra-ui/react';
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
 import PageContainer from 'layout/PageContainer';
 
 const UserProfile: React.FC = () => {
+  const {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    user: { addr },
+  } = useWebContext();
+
+  const isBiggerThanMobile = useMediaQuery();
+
   return (
     <PageContainer>
-      {
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin finibus nisi mauris, ut accumsan diam rutrum non. Pellentesque ac pretium urna. Praesent scelerisque mollis nisl, non imperdiet odio rutrum id. Donec fringilla rutrum mi sed pulvinar. Vestibulum sed sem posuere, sagittis nibh in, ullamcorper nibh. Suspendisse vehicula, purus nec facilisis luctus, mauris dui dapibus massa, sed convallis tortor elit vitae magna.'
-      }
+      <Flex width="100%" flexWrap={['wrap', null, 'nowrap']}>
+        <Flex maxWidth={[null, null, '400px']} width="100%" flexWrap="wrap">
+          <Flex minW="100%">
+            <Blockies
+              seed={addr}
+              size={10}
+              scale={isBiggerThanMobile ? 12.4 : 9.6}
+              className="blockies"
+            />
+          </Flex>
+          <Flex pt="24px" minW="100%">
+            <Box maxW="193px">
+              <WalletAddress addr={addr} />
+            </Box>
+          </Flex>
+          <Flex bg="blue" minW="100%">
+            Buttons
+          </Flex>
+          <Flex minW="100%">
+            <CommunityLinks
+              instagramUrl="dfdf"
+              twitterUrl="dfdf"
+              websiteUrl="dfdf"
+              discordUrl="dfdf"
+              githubUrl="dfdf"
+            />
+          </Flex>
+        </Flex>
+        {/* <Spacer minWidth={[0, 0, '72px']} /> */}
+        <Flex flexGrow={1}>
+          <Tabs>
+            <TabList>
+              <Tab>Activity</Tab>
+              <Tab>Communities</Tab>
+              <Tab>Memberships</Tab>
+            </TabList>
+
+            <TabPanels>
+              <TabPanel>
+                <p>All Activities</p>
+              </TabPanel>
+              <TabPanel>
+                <p>Communities!</p>
+              </TabPanel>
+              <TabPanel>
+                <p>Memberships</p>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Flex>
+      </Flex>
     </PageContainer>
   );
 };
